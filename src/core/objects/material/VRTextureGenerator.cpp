13c8f278e858133ec46cc038ec2c206311c8a1fa
Victor <victor.haefner@gmail.com>
2014-10-23 18:54:48 +0200

    new class, VRTextureGenerator

---
+++ src/core/objects/material/VRTextureGenerator.cpp
@@ -0,0 +1,15 @@
+#include "VRTextureGenerator.h"
+
+OSG_BEGIN_NAMESPACE;
+using namespace std;
+
+VRTextureGenerator::VRTextureGenerator() {}
+
+void VRTextureGenerator::setSize(int w, int h) {}
+
+void VRTextureGenerator::addNoise(NOISE_TYPE noise, float amount) {}
+void VRTextureGenerator::clearStage() {}
+
+ImageRecPtr VRTextureGenerator::compose(int seed) { }
+
+OSG_END_NAMESPACE;
---
+++ src/core/objects/material/VRTextureGenerator.h
@@ -0,0 +1,31 @@
+#ifndef VRTEXTUREGENERATOR_H_INCLUDED
+#define VRTEXTUREGENERATOR_H_INCLUDED
+
+#include <OpenSG/OSGFieldContainerFields.h>
+
+OSG_BEGIN_NAMESPACE;
+using namespace std;
+
+class Image; OSG_GEN_CONTAINERPTR(Image);
+
+enum NOISE_TYPE {
+    PERLIN
+};
+
+class VRTextureGenerator {
+    private:
+
+    public:
+        VRTextureGenerator();
+
+        void setSize(int w, int h);
+
+        void addNoise(NOISE_TYPE noise, float amount);
+        void clearStage();
+
+        ImageRecPtr compose(int seed);
+};
+
+OSG_END_NAMESPACE;
+
+#endif // VRTEXTUREGENERATOR_H_INCLUDED
