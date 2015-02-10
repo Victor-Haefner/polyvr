75c7e05fedda24cae515fc0807b395c7e5098610
Victor <victor.haefner@gmail.com>
2015-02-10 01:50:07 +0100

    refactored sound

--- src/core/scene/VRSoundManager.cpp
+++ src/core/scene/VRSoundManager.cpp
@@ -14,8 +14,6 @@ extern "C" {
 #include <boost/thread/thread.hpp>
 #include <boost/thread.hpp>
 
-boost::mutex t_mutex;
-
 OSG_BEGIN_NAMESPACE;
 using namespace std;
 
@@ -30,39 +28,83 @@ struct VRSound {
     AVFormatContext* context = NULL;
     AVCodecContext* codec = NULL;
     bool interrupt = false;
+    bool initiated = false;
 
-    VRSound(bool loop) {
-        //context = avformat_alloc_context();
+    bool loop;
+    float pitch, gain;
+    Vec3f pos, vel;
 
-        alGenBuffers(1u, &buffer);
-        alGenSources(1u, &source);
-
-        alSourcef(source, AL_PITCH, 1);
-        alSourcef(source, AL_GAIN, 1);
-        alSource3f(source, AL_POSITION, 0, 0, 0);
-        alSource3f(source, AL_VELOCITY, 0, 0, 0);
-        alSourcei(source, AL_LOOPING, (loop ? AL_TRUE : AL_FALSE));
-    }
+    VRSound() {}
 
     ~VRSound() {
         alDeleteSources(1u, &source);
         alDeleteBuffers(1u, &buffer);
         avformat_close_input(&context);
     }
-};
 
-struct VRSoundJob {
-    string path;
-    bool looping;
-
-    VRSoundJob(string filename, bool loop) {
-        path = filename;
-        looping = loop;
+    void setLoop(bool loop) {
+        this->loop = loop;
+        alSourcei(source, AL_LOOPING, (loop ? AL_TRUE : AL_FALSE));
     }
 
-    /*~VRSoundJob() {
-        delete path;
-    }*/
+    void setPitch(float pitch) {
+        this->pitch = pitch;
+        alSourcef(source, AL_PITCH, pitch);
+    }
+
+    void setGain(float gain) {
+        this->gain = gain;
+        alSourcef(source, AL_GAIN, gain);
+    }
+
+    void setUser(Vec3f p, Vec3f v) {
+        pos = p;
+        vel = v;
+        alSource3f(source, AL_POSITION, p[0], p[1], p[2]);
+        alSource3f(source, AL_VELOCITY, v[0], v[1], v[2]);
+    }
+
+    bool initiate() {
+        if (initiated) return initiated;
+        else initiated = true;
+        alGenBuffers(1u, &buffer);
+        alGenSources(1u, &source);
+        setPitch(1);
+        setGain(1);
+        setUser(Vec3f(), Vec3f());
+
+        if (avformat_open_input(&context, path.c_str(), NULL, NULL) < 0) return 0;
+        if (avformat_find_stream_info(context, NULL) < 0) return 0;
+
+        int stream_id = -1;
+        for (uint i = 0; i < context->nb_streams; i++) {
+            if (context->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO) {
+                stream_id = i;
+                break;
+            }
+        }
+        if (stream_id == -1) return 0;
+
+        this->stream_id = stream_id;
+        codec = context->streams[stream_id]->codec;
+        AVCodec* avcodec = avcodec_find_decoder(codec->codec_id);
+        if (avcodec == 0) return 0;
+
+        if (avcodec_open2(codec, avcodec, NULL) < 0) return 0;
+
+        frequency = codec->sample_rate;
+        format = AL_FORMAT_MONO16;
+        AVSampleFormat sfmt = codec->sample_fmt;
+
+        if (sfmt == AV_SAMPLE_FMT_U8) format = AL_FORMAT_MONO8;
+        if (sfmt == AV_SAMPLE_FMT_S16) format = AL_FORMAT_MONO16;
+
+        if (codec->channels == 2) {
+            if (sfmt == AV_SAMPLE_FMT_U8) format = AL_FORMAT_STEREO8;
+            if (sfmt == AV_SAMPLE_FMT_S16) format = AL_FORMAT_STEREO16;
+        }
+        return true;
+    }
 };
 
 struct VRSoundContext {
@@ -71,16 +113,10 @@ struct VRSoundContext {
 
     VRSoundContext() {
         av_register_all();
-
         device = alcOpenDevice(NULL);
-
-        if (!device) {
-            cout << "VRSoundContext() > alcOpenDevice failed!\n";
-            return;
-        }
+        if (!device) { cout << "VRSoundContext() > alcOpenDevice failed!\n"; return; }
 
         context = alcCreateContext(device, NULL);
-
         if (!alcMakeContextCurrent(context)) cout << "VRSoundContext() > alcMakeContextCurrent failed!\n";
     }
 
@@ -91,225 +127,178 @@ struct VRSoundContext {
     }
 };
 
-VRSoundManager::VRSoundManager() {
-    s_thread = new boost::thread(boost::bind(&VRSoundManager::soundThread, this));
-}
+struct VRSoundChannel {
+    bool running = true;
+    bool busy = false;
+    boost::thread* thread;
+    VRSoundContext* context;
+    boost::mutex mutex;
+    VRSound* current = 0;
 
-void VRSoundManager::soundThread() {
-    context = new VRSoundContext();
-
-    while (t_running) {
-        vector<VRSoundJob*> sounds;
-        {
-            boost::mutex::scoped_lock lock(t_mutex);
-            sounds = soundJobs;
-            soundJobs.clear();
-        }
-
-        for (auto job : sounds) {
-            VRSound* sound = getSound(job);
-            play(sound);
-        }
-
-        osgSleep(5);
+    VRSoundChannel() {
+        thread = new boost::thread(boost::bind(&VRSoundChannel::soundThread, this));
     }
 
-    clearSoundMap();
+    ~VRSoundChannel() {
+        running = false;
+        thread->join();
+        delete thread;
+    }
 
-    if (bgMusic) delete bgMusic;
+    void play(VRSound* sound) {
+        boost::mutex::scoped_lock lock(mutex);
+        current = sound;
+    }
 
-    if (context) delete context;
-}
+    void soundThread() {
+        context = new VRSoundContext();
 
+        while (running) {
+            osgSleep(1);
+
+            {
+                boost::mutex::scoped_lock lock(mutex);
+                if (current == 0) continue;
+            }
+
+            cout << "PLAY " << current->loop << endl;
+            if (current->initiate()) stream(current);
+            if (!current->loop) {
+                boost::mutex::scoped_lock lock(mutex);
+                current = 0;
+            }
+        }
+
+        if (context) delete context;
+    }
+
+    void stream(VRSound* sound) {
+        if (sound == 0) return;
+
+        cout << "stream " << sound << " from " << this << endl;
+        busy = true;
+
+        // libav
+        AVPacket packet;
+        AVFrame* decodedFrame;
+        decodedFrame = avcodec_alloc_frame();
+
+        int len;
+
+        while (1) {
+            if ((sound->interrupt) || (av_read_frame(sound->context, &packet) < 0)) { cout << "end of stream\n"; break; } // End of stream. Done decoding.
+
+            if (packet.stream_index != sound->stream_id) { cout << "skip non audio\n"; continue; } // Skip non audio packets
+
+            while (packet.size > 0) { // Decodes audio data from `packet` into the frame
+                if (sound->interrupt) { cout << "interrupt sound\n"; break; }
+
+                int finishedFrame = 0;
+                len = avcodec_decode_audio4(sound->codec, decodedFrame, &finishedFrame, &packet);
+
+                if (len < 0) { cout << "decoding error\n"; break; }
+
+                if (finishedFrame) {
+                    if (sound->interrupt) { cout << "interrupt sound\n"; break; }
+
+                    // Decoded data is now available in decodedFrame->data[0]
+                    int data_size = av_samples_get_buffer_size(NULL, sound->codec->channels, decodedFrame->nb_samples, sound->codec->sample_fmt, 1);
+
+                    // OpenAL consumes buffers in the background
+                    // we first need to initialize the OpenAL buffers then
+                    // start continous playback.
+                    if (sound->init) {
+                        alBufferData(sound->buffer, sound->format, decodedFrame->data[0], data_size, sound->frequency);
+                        alSourceQueueBuffers(sound->source, 1, &sound->buffer);    // all buffers queued
+                        alSourcePlay(sound->source);    // start playback
+                        sound->init = 0;
+                    } else {
+                        ALuint buffer;
+                        ALint val = -1;
+
+                        while (val <= 0) alGetSourcei(sound->source, AL_BUFFERS_PROCESSED, &val);      // wait for openal to release one buffer
+
+                        // fill and requeue the empty buffer
+                        alSourceUnqueueBuffers(sound->source, 1, &buffer);
+                        alBufferData(buffer, sound->format, decodedFrame->data[0], data_size, sound->frequency);
+                        alSourceQueueBuffers(sound->source, 1, &buffer);
+
+                        //Restart openal playback if needed
+                        alGetSourcei(sound->source, AL_SOURCE_STATE, &val);
+
+                        if (val != AL_PLAYING) alSourcePlay(sound->source);
+                    }
+                }
+
+                //There may be more than one frame of audio data inside the packet.
+                packet.size -= len;
+                packet.data += len;
+            } // while packet.size > 0
+        } // while more packets exist inside container.
+
+        if (packet.data) av_free_packet(&packet);
+        sound->init = 1;
+        cout << "stream done " << sound->path << endl;
+        busy = false;
+    }
+};
+
+VRSoundManager::VRSoundManager() {}
+VRSoundManager::~VRSoundManager() { clearSoundMap(); }
 VRSoundManager& VRSoundManager::get() {
     static VRSoundManager* instance = new VRSoundManager();
     return *instance;
 }
 
-VRSoundManager::~VRSoundManager() {
-    t_running = false;
-    s_thread->join();
-}
-
 void VRSoundManager::clearSoundMap() {
     for (auto sound : sounds) if (sound.second) delete sound.second;
-
     sounds.clear();
 }
 
-void VRSoundManager::playSound(string filename, bool loop) {
-    boost::mutex::scoped_lock lock(t_mutex);
-    soundJobs.push_back(new VRSoundJob(filename, loop));
-}
+void VRSoundManager::playSound(string path, bool loop) {
+    VRSound* sound = getSound(path);
+    sound->setLoop(loop);
 
-void VRSoundManager::playSound(string filename) {
-    playSound(filename, false);
-}
-
-void VRSoundManager::play(VRSound* sound) {
-    if (sound == 0) return;
-
-    cout << "playSound " << sound->path << endl;
-
-    // libav
-    AVPacket packet;
-    AVFrame* decodedFrame;
-    decodedFrame = avcodec_alloc_frame();
-
-    int len;
-
-    while (1) {
-        if ((sound->interrupt) || (av_read_frame(sound->context, &packet) < 0)) break; // End of stream. Done decoding.
-
-        if (packet.stream_index != sound->stream_id) continue; // Skip non audio packets
-
-        while (packet.size > 0) { // Decodes audio data from `packet` into the frame
-            if (sound->interrupt) break;
-
-            int finishedFrame = 0;
-            len = avcodec_decode_audio4(sound->codec, decodedFrame, &finishedFrame, &packet);
-
-            if (len < 0) {
-                printf("error"); // Error in decoding
-                break;
-            }
-
-            if (finishedFrame) {
-                if (sound->interrupt) break;
-
-                // Decoded data is now available in decodedFrame->data[0]
-                int data_size = av_samples_get_buffer_size(NULL, sound->codec->channels, decodedFrame->nb_samples, sound->codec->sample_fmt, 1);
-
-                // OpenAL consumes buffers in the background
-                // we first need to initialize the OpenAL buffers then
-                // start continous playback.
-                if (sound->init) {
-                    alBufferData(sound->buffer, sound->format, decodedFrame->data[0], data_size, sound->frequency);
-                    alSourceQueueBuffers(sound->source, 1, &sound->buffer);    // all buffers queued
-                    alSourcePlay(sound->source);    // start playback
-                    sound->init = 0;
-                }
-                else {
-                    ALuint buffer;
-                    ALint val = -1;
-
-                    while (val <= 0) alGetSourcei(sound->source, AL_BUFFERS_PROCESSED, &val);      // wait for openal to release one buffer
-
-                    // fill and requeue the empty buffer
-                    alSourceUnqueueBuffers(sound->source, 1, &buffer);
-                    alBufferData(buffer, sound->format, decodedFrame->data[0], data_size, sound->frequency);
-                    alSourceQueueBuffers(sound->source, 1, &buffer);
-
-                    //Restart openal playback if needed
-                    alGetSourcei(sound->source, AL_SOURCE_STATE, &val);
-
-                    if (val != AL_PLAYING) alSourcePlay(sound->source);
-                }
-            }
-
-            //There may be more than one frame of audio data inside the packet.
-            packet.size -= len;
-            packet.data += len;
-        } // while packet.size > 0
-    } // while more packets exist inside container.
-
-    if (packet.data) av_free_packet(&packet);
-}
-
-VRSound* VRSoundManager::getSound(VRSoundJob* soundJob) {
-    cout << "getSound " << soundJob->path << endl;
-
-    VRSound* sound = new VRSound(soundJob->looping);
-    sound->path = soundJob->path;
-
-    if (avformat_open_input(&sound->context, sound->path.c_str(), NULL, NULL) < 0) return 0;
-
-    if (avformat_find_stream_info(sound->context, NULL) < 0) return 0;
-
-    int stream_id = -1;
-
-    for (uint i = 0; i < sound->context->nb_streams; i++) {
-        if (sound->context->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO) {
-            stream_id = i;
-            break;
-        }
+    VRSoundChannel* channel = 0;
+    for (auto c : channels) if (!c->busy) { channel = c; break; }
+    if (channel == 0) {
+        channel = new VRSoundChannel();
+        channels.push_back(channel);
     }
+    channel->play(sound);
+}
 
-    if (stream_id == -1) return 0;
-
-    sound->stream_id = stream_id;
-
-    sound->codec = sound->context->streams[stream_id]->codec;
-    AVCodec* codec = avcodec_find_decoder(sound->codec->codec_id);
-
-    if (codec == 0) return 0;
-
-    if (avcodec_open2(sound->codec, codec, NULL) < 0) return 0;
-
-    sound->frequency = sound->codec->sample_rate;
-    sound->format = AL_FORMAT_MONO16;
-    AVSampleFormat sfmt = sound->codec->sample_fmt;
-
-    if (sfmt == AV_SAMPLE_FMT_U8) sound->format = AL_FORMAT_MONO8;
-
-    if (sfmt == AV_SAMPLE_FMT_S16) sound->format = AL_FORMAT_MONO16;
-
-    if (sound->codec->channels == 2) {
-        if (sfmt == AV_SAMPLE_FMT_U8) sound->format = AL_FORMAT_STEREO8;
-
-        if (sfmt == AV_SAMPLE_FMT_S16) sound->format = AL_FORMAT_STEREO16;
-    }
-
+VRSound* VRSoundManager::getSound(string path) {
+    VRSound* sound = new VRSound();
+    sound->path = path;
     sounds[sound->path] = sound;
     return sound;
 }
 
-void VRSoundManager::playMusic(string filename) { }
-
-void VRSoundManager::stopMusic() { }
-
 void VRSoundManager::setSoundVolume(float volume) {
-    if (volume > 1.0f) volume = 1.0f;
-    else
-        if (volume < 0.0f) volume = 0.0f;
+    volume = max(volume, 0.f);
+    volume = min(volume, 1.f);
 
-    for (auto iterator = sounds.begin(); iterator != sounds.end(); iterator++) {
-        VRSound* sound = iterator->second;
-        cout << "setVolume " << sound->path << endl;
-        alSourcef(sound->source, AL_GAIN, volume);
-    }
-}
+    for (auto s : sounds) alSourcef(s.second->source, AL_GAIN, volume);
 
-void VRSoundManager::setMusicVolume(float volume) {
     // This is just working with Ubuntu!
-    if (volume > 1.0) volume = 1.0f;
-    else
-        if (volume < 0.0) volume = 0.0f;
-
-    string percent = boost::lexical_cast<std::string>(static_cast<int>(volume * 100));
-    string amixer = "amixer -D pulse sset Master " + percent + "% > /dev/null 2>&1";
-    system(amixer.c_str());
+    //string percent = boost::lexical_cast<std::string>(static_cast<int>(volume * 100));
+    //string amixer = "amixer -D pulse sset Master " + percent + "% > /dev/null 2>&1";
+    //system(amixer.c_str());
 }
 
 /*void VRSoundManager::updatePlayerPosition(Vec3f position, Vec3f forward) { }*/
 
 void VRSoundManager::stopSound(string path) {
-    for (auto iterator : sounds) {
-        VRSound* sound = iterator.second;
-
-        if (sound->path.compare(path) == 0) {
-            cout << "stopSound " << sound->path << endl;
-            sound->interrupt = true;
-        }
+    for (auto s : sounds) {
+        if (s.second->path != path) continue;
+        s.second->interrupt = true;
+        return;
     }
 }
 
 void VRSoundManager::stopAllSounds(void) {
-    for (auto iterator : sounds) {
-        VRSound* sound = iterator.second;
-        cout << "stopSound " << sound->path << endl;
-        sound->interrupt = true;
-    }
+    for (auto s : sounds) s.second->interrupt = true;
 }
 
 OSG_END_NAMESPACE;
--- src/core/scene/VRSoundManager.h
+++ src/core/scene/VRSoundManager.h
@@ -4,31 +4,22 @@
 #include <OpenSG/OSGVector.h>
 #include <map>
 
-namespace boost {
-class thread;
-}
+namespace boost { class thread; }
 
 using namespace std;
 OSG_BEGIN_NAMESPACE;
 
 struct VRSound;
-struct VRSoundJob;
-struct VRSoundContext;
+struct VRSoundChannel;
 
 class VRSoundManager {
-private:
-    VRSoundContext* context = 0;
-    VRSound* bgMusic = 0;
+public:
     map<string, VRSound*> sounds;
-    vector<VRSoundJob*> soundJobs;
+    vector<VRSoundChannel*> channels;
 
     VRSoundManager();
-    VRSound* getSound(VRSoundJob* soundJob);
+    VRSound* getSound(string path);
     void clearSoundMap(void);
-
-    bool t_running = true;
-    boost::thread* s_thread = 0;
-    void soundThread();
 
     void play(VRSound* sound);
 
@@ -36,19 +27,13 @@ public:
     static VRSoundManager& get();
     ~VRSoundManager();
 
-    void playMusic(string filename);
+    void playSound(string path, bool loop = false);
+    void playPositionalSound(string path, Vec3f vec);
+
     void stopSound(string path);
-    void stopMusic(void);
-
-    void playSound(string filename);
-    void playSound(string filename, bool loop);
-    void playPositionalSound(string filename, Vec3f vec);
-
     void stopAllSounds(void);
 
-    void setMusicVolume(float volume);
     void setSoundVolume(float volume);
-
     void updatePlayerPosition(Vec3f position, Vec3f forward);
 };
 
