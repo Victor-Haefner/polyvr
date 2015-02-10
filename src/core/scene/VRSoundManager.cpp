a7c7b223590958adc2e7aff9bef6db4da65d345e
Victor <victor.haefner@gmail.com>
2015-02-10 11:22:47 +0100

    added OpenAL error checks

--- src/core/scene/VRSoundManager.cpp
+++ src/core/scene/VRSoundManager.cpp
@@ -10,16 +10,32 @@ extern "C" {
 
 #include <AL/al.h>
 #include <AL/alc.h>
+//#include <AL/alut.h>
 
 #include <boost/thread/thread.hpp>
 #include <boost/thread.hpp>
 
+#define ALCHECK(x) { \
+x; \
+ALenum error = alGetError(); \
+if (error != AL_NO_ERROR) cout << "AL error: " << toString(error) << endl; \
+}
+
 OSG_BEGIN_NAMESPACE;
 using namespace std;
 
+string toString(ALenum error) {
+    if(error == AL_INVALID_NAME) return "Invalid name";
+    if(error == AL_INVALID_ENUM) return "Invalid enum ";
+    if(error == AL_INVALID_VALUE) return "Invalid value ";
+    if(error == AL_INVALID_OPERATION) return "Invalid operation ";
+    if(error == AL_OUT_OF_MEMORY) return "Out of memory";
+    return "Unknown error";
+}
+
 struct VRSound {
     uint source;
-    uint buffer;
+    uint* buffers;
     uint frequency;
     uint Nbuffers = 1;
     int stream_id;
@@ -35,37 +51,40 @@ struct VRSound {
     float pitch, gain;
     Vec3f pos, vel;
 
-    VRSound() {}
+    VRSound() {
+        buffers = new uint[Nbuffers];
+    }
 
     ~VRSound() {
         close();
+        delete buffers;
     }
 
     void setLoop(bool loop) {
         this->loop = loop;
-        alSourcei(source, AL_LOOPING, (loop ? AL_TRUE : AL_FALSE));
+        ALCHECK( alSourcei(source, AL_LOOPING, (loop ? AL_TRUE : AL_FALSE)) );
     }
 
     void setPitch(float pitch) {
         this->pitch = pitch;
-        alSourcef(source, AL_PITCH, pitch);
+        ALCHECK( alSourcef(source, AL_PITCH, pitch));
     }
 
     void setGain(float gain) {
         this->gain = gain;
-        alSourcef(source, AL_GAIN, gain);
+        ALCHECK( alSourcef(source, AL_GAIN, gain));
     }
 
     void setUser(Vec3f p, Vec3f v) {
         pos = p;
         vel = v;
-        alSource3f(source, AL_POSITION, p[0], p[1], p[2]);
-        alSource3f(source, AL_VELOCITY, v[0], v[1], v[2]);
+        ALCHECK( alSource3f(source, AL_POSITION, p[0], p[1], p[2]));
+        ALCHECK( alSource3f(source, AL_VELOCITY, v[0], v[1], v[2]));
     }
 
     void close() {
-        alDeleteSources(1u, &source);
-        alDeleteBuffers(1u, &buffer);
+        ALCHECK( alDeleteSources(1u, &source));
+        ALCHECK( alDeleteBuffers(Nbuffers, buffers));
         avformat_close_input(&context);
     }
 
@@ -73,8 +92,8 @@ struct VRSound {
         if (initiated) close();
         initiated = true;
 
-        alGenBuffers(Nbuffers, &buffer);
-        alGenSources(1u, &source);
+        ALCHECK( alGenBuffers(Nbuffers, buffers) );
+        ALCHECK( alGenSources(1u, &source) );
         setPitch(1);
         setGain(1);
         setUser(Vec3f(), Vec3f());
@@ -137,25 +156,23 @@ struct VRSound {
                     int data_size = av_samples_get_buffer_size(NULL, codec->channels, decodedFrame->nb_samples, codec->sample_fmt, 1);
 
                     if (init) { // initialize OpenAL buffers
-                        alBufferData(buffer, format, decodedFrame->data[0], data_size, frequency);
-                        alSourceQueueBuffers(source, 1, &buffer);    // all buffers queued
-                        alSourcePlay(source);    // start playback
+                        ALCHECK( alBufferData(buffers[0], format, decodedFrame->data[0], data_size, frequency));
+                        ALCHECK( alSourceQueueBuffers(source, Nbuffers, buffers));    // all buffers queued
+                        ALCHECK( alSourcePlay(source));    // start playback
                         init = 0;
                     } else { // start continous playback.
-                        ALuint buffer;
+                        //ALuint buffer;
                         ALint val = -1;
-
-                        while (val <= 0) alGetSourcei(source, AL_BUFFERS_PROCESSED, &val);      // wait for openal to release one buffer
+                        while (val <= 0) ALCHECK( alGetSourcei(source, AL_BUFFERS_PROCESSED, &val));      // wait for openal to release one buffer
 
                         // fill and requeue the empty buffer
-                        alSourceUnqueueBuffers(source, 1, &buffer);
-                        alBufferData(buffer, format, decodedFrame->data[0], data_size, frequency);
-                        alSourceQueueBuffers(source, 1, &buffer);
+                        ALCHECK( alSourceUnqueueBuffers(source, 1, buffers+val));
+                        ALCHECK( alBufferData(buffers[val], format, decodedFrame->data[0], data_size, frequency));
+                        ALCHECK( alSourceQueueBuffers(source, 1, buffers+val));
 
                         //Restart openal playback if needed
-                        alGetSourcei(source, AL_SOURCE_STATE, &val);
-
-                        if (val != AL_PLAYING) alSourcePlay(source);
+                        ALCHECK( alGetSourcei(source, AL_SOURCE_STATE, &val));
+                        if (val != AL_PLAYING) ALCHECK( alSourcePlay(source));
                     }
                 }
 
@@ -182,12 +199,23 @@ struct VRSoundContext {
 
         context = alcCreateContext(device, NULL);
         if (!alcMakeContextCurrent(context)) cout << "VRSoundContext() > alcMakeContextCurrent failed!\n";
+
+        alGetError();
+        //enumerateDevices();
     }
 
     ~VRSoundContext() {
         alcMakeContextCurrent(NULL);
         alcDestroyContext(context);
         alcCloseDevice(device);
+    }
+
+    void enumerateDevices() {
+        bool enumeration = alcIsExtensionPresent(NULL, "ALC_ENUMERATION_EXT");
+        if (enumeration) {
+            cout << "Enumerate OpenAL devices:\n";
+            cout << string( alcGetString(NULL, ALC_DEVICE_SPECIFIER) ) << endl;
+        }
     }
 };
 
@@ -239,7 +267,10 @@ struct VRSoundChannel {
     }
 };
 
-VRSoundManager::VRSoundManager() {}
+VRSoundManager::VRSoundManager() {
+    ;
+}
+
 VRSoundManager::~VRSoundManager() { clearSoundMap(); }
 VRSoundManager& VRSoundManager::get() {
     static VRSoundManager* instance = new VRSoundManager();
