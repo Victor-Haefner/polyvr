b94d09fc89a468b9be65b357588248d314364ed2
Victor <victor.haefner@gmail.com>
2015-02-17 00:44:38 +0100

    working on segmentation

--- src/addons/Classification/VRSegmentation.cpp
+++ src/addons/Classification/VRSegmentation.cpp
@@ -33,12 +33,12 @@ struct VRPlane {
 
 Vec3f randC() {return Vec3f(0.5+0.5*rand()/RAND_MAX, 0.5+0.5*rand()/RAND_MAX, 0.5+0.5*rand()/RAND_MAX); }
 
-struct accu_bin {
-    float weight = 0;
-    VRPlane plane;
-};
+struct Accumulator {
+    struct Bin {
+        float weight = 0;
+        VRPlane plane;
+    };
 
-struct Accumulator_base {
     virtual void pushPoint(Pnt3f p) = 0;
 
     void pushGeometry(VRGeometry* geo_in) {
@@ -56,36 +56,38 @@ struct Accumulator_base {
             pushPoint(p);
         }
     }
+
+    virtual vector<VRPlane> eval() = 0;
 };
 
-struct Accumulator : public Accumulator_base {
-    accu_bin* data;
+struct Accumulator_grid : public Accumulator {
+    Bin* data;
     int Da, Dr;
     float Ra, Rr, Dp;
 
-    Accumulator(int Da, int Dr) {
+    Accumulator_grid(int Da, int Dr) {
         this->Da = Da;
         this->Dr = Dr;
         Ra = Pi/Da;
         Rr = 5.0/Dr;
         Dp = 0.05*Rr;
-        data = new accu_bin[size()];
+        data = new Bin[size()];
         clear();
 
         for (int th=0; th < Da; th++) {
             for (int phi=0; phi < Da; phi++) {
                 for (int rho=-Dr; rho < Dr; rho++) {
-                    (*this)(th, phi, rho).plane = VRPlane(th/Ra, phi/Ra, rho/Rr);
+                    (*this)(th, phi, rho).plane = VRPlane(th*Ra, phi*Ra, rho*Rr);
                 }
             }
         }
     }
 
-    ~Accumulator() {
+    ~Accumulator_grid() {
         delete[] data;
     }
 
-    void clear() { memset(data, 0, size()*sizeof(accu_bin)); }
+    void clear() { memset(data, 0, size()*sizeof(Bin)); }
 
     void pushPoint(Pnt3f p) {
         for (int j = 0; j < size(); j++) {
@@ -93,21 +95,33 @@ struct Accumulator : public Accumulator_base {
         }
     }
 
-    accu_bin& operator()(int i, int j, int k) { return data[i*Da*Dr*2 + j*Dr*2 + k + Dr]; }
+    Bin& operator()(int i, int j, int k) { return data[i*Da*Dr*2 + j*Dr*2 + k + Dr]; }
 
     int size() { return 2*Dr*Da*Da; }
 
-    Vec4f plane(float theta, float phi, float rho) {
-        float th = Ra*theta - Pi*0.5;
-        float ph = Ra*phi;
-        float sin_th = sin(th);
-        return Vec4f(cos(ph)*sin_th, cos(th), sin(ph)*sin_th, -rho*Rr);
-    }
+    vector<VRPlane> eval() {
+        map<int, vector<VRPlane> > res; // the planes by importance
+        for (int i=0; i < Da; i++) {
+            for (int j=0; j < Da; j++) {
+                for (int k=-Dr; k < Dr; k++) {
+                    int a = (*this)(i, j, k).weight;
+                    if (res.count(a) == 0) res[a] = vector<VRPlane>();
+                    res[a].push_back( (*this)(i, j, k).plane );
+                }
+            }
+        }
 
-    bool pOnPlane(Pnt3f& p, Vec4f& pl, float& th) { return abs(pl[0]*p[0]+pl[1]*p[1]+pl[2]*p[2]+pl[3]) < th; }
+        vector<VRPlane> planes;
+        for (auto pvec : boost::adaptors::reverse(res)) {
+            if (pvec.first == 0) continue;
+            for (auto m : pvec.second) planes.push_back(m);
+        }
+
+        return planes;
+    }
 };
 
-struct Accumulator_octree : public Accumulator_base {
+struct Accumulator_octree : public Accumulator {
     Octree* octree;
     int Da, Dr;
     float Ra, Rr, Dp;
@@ -125,13 +139,11 @@ struct Accumulator_octree : public Accumulator_base {
         delete octree;
     }
 
-    void clear() { ; }
-
     float getWeight(Vec3f p) {
         Octree* t = octree->get(p[0], p[1], p[2]);
         vector<void*> data = t->getData();
         if (data.size() == 0) return 0;
-        accu_bin* bin = (accu_bin*)data[0];
+        Bin* bin = (Bin*)data[0];
         return bin->weight;
     }
 
@@ -140,67 +152,20 @@ struct Accumulator_octree : public Accumulator_base {
         Octree* t = octree->get(p[0], p[1], p[2]);
         vector<void*> data = t->getData();
         if (data.size() > 0) {
-            accu_bin* bin = (accu_bin*)data[0];
+            Bin* bin = (Bin*)data[0];
             w = bin->weight;
             bin->weight += 0.1;
         }
 
-        t->add(p[0], p[1], p[2], new accu_bin(), w);
+        t->add(p[0], p[1], p[2], new Bin(), w);
     }
 
-    Vec4f plane(float theta, float phi, float rho) {
-        float th = Ra*theta - Pi*0.5;
-        float ph = Ra*phi;
-        float sin_th = sin(th);
-        return Vec4f(cos(ph)*sin_th, cos(th), sin(ph)*sin_th, -rho*Rr);
+    vector<VRPlane> eval() {
+        ;
     }
-
-    bool pOnPlane(Pnt3f& p, Vec4f& pl, float& th) { return abs(pl[0]*p[0]+pl[1]*p[1]+pl[2]*p[2]+pl[3]) < th; }
 };
 
-vector<Vec4f> evalAccumulatorOctree(Accumulator_octree& accu) {
-    /*map<int, vector<Vec3f> > accu_results; // the planes by importance
-    for (int i=0; i < accu.Da; i++) {
-        for (int j=0; j < accu.Da; j++) {
-            for (int k=-accu.Dr; k < accu.Dr; k++) {
-                int a = accu(i, j, k).weight;
-                if (accu_results.count(a) == 0) accu_results[a] = vector<Vec3f>();
-                accu_results[a].push_back( Vec3f(i,j,k) );
-            }
-        }
-    }
-
-    vector<Vec4f> planes;
-    for (auto pvec : boost::adaptors::reverse(accu_results)) {
-        if (pvec.first == 0) continue;
-        for (auto m : pvec.second) planes.push_back(accu.plane(m[0], m[1], m[2]));
-    }
-
-    return planes;*/
-}
-
-vector<Vec4f> evalAccumulator(Accumulator& accu) {
-    map<int, vector<Vec3f> > accu_results; // the planes by importance
-    for (int i=0; i < accu.Da; i++) {
-        for (int j=0; j < accu.Da; j++) {
-            for (int k=-accu.Dr; k < accu.Dr; k++) {
-                int a = accu(i, j, k).weight;
-                if (accu_results.count(a) == 0) accu_results[a] = vector<Vec3f>();
-                accu_results[a].push_back( Vec3f(i,j,k) );
-            }
-        }
-    }
-
-    vector<Vec4f> planes;
-    for (auto pvec : boost::adaptors::reverse(accu_results)) {
-        if (pvec.first == 0) continue;
-        for (auto m : pvec.second) planes.push_back(accu.plane(m[0], m[1], m[2]));
-    }
-
-    return planes;
-}
-
-vector<VRGeometry*> extractPointsOnPlane(VRGeometry* geo_in, Vec4f plane, Accumulator& accu, float Dp) {
+vector<VRGeometry*> extractPointsOnPlane(VRGeometry* geo_in, VRPlane plane, float Dp) {
     GeoVectorPropertyRecPtr positions = geo_in->getMesh()->getPositions();
 
     vector<VRGeometry*> res; // two geometries, the plane and the rest
@@ -214,42 +179,7 @@ vector<VRGeometry*> extractPointsOnPlane(VRGeometry* geo_in, Vec4f plane, Accumu
     Pnt3f p;
     for (uint i = 0; i<positions->size(); i++) {
         positions->getValue(p, i);
-        if (accu.pOnPlane(p, plane, Dp)) res[0]->getMesh()->getPositions()->addValue(p);
-        else res[1]->getMesh()->getPositions()->addValue(p);
-    }
-
-    for (auto geo : res) {
-        geo->setType(GL_POINTS);
-        GeoUInt32PropertyRecPtr inds = GeoUInt32Property::create();
-        for (uint i=0; i<geo->getMesh()->getPositions()->size(); i++) inds->addValue(i);
-        geo->setIndices(inds);
-        geo->setMaterial(new VRMaterial("pmat"));
-
-        //geo->getMaterial()->setDiffuse(Vec3f(pl));
-        geo->getMaterial()->setDiffuse(randC());
-        geo->getMaterial()->setPointSize(5);
-        geo->getMaterial()->setLit(false);
-        geo->addAttachment("dynamicaly_generated", 0);
-    }
-
-    return res;
-}
-
-vector<VRGeometry*> extractPointsOnPlaneOctree(VRGeometry* geo_in, Vec4f plane, Accumulator_octree& accu, float Dp) {
-    GeoVectorPropertyRecPtr positions = geo_in->getMesh()->getPositions();
-
-    vector<VRGeometry*> res; // two geometries, the plane and the rest
-    for (int i=0; i<2; i++) {
-        VRGeometry* geo = new VRGeometry(geo_in->getName() + "_p");
-        GeoPnt3fPropertyRecPtr pos = GeoPnt3fProperty::create();
-        geo->setPositions(pos);
-        res.push_back(geo);
-    }
-
-    Pnt3f p;
-    for (uint i = 0; i<positions->size(); i++) {
-        positions->getValue(p, i);
-        if (accu.pOnPlane(p, plane, Dp)) res[0]->getMesh()->getPositions()->addValue(p);
+        if (plane.on(p, Dp)) res[0]->getMesh()->getPositions()->addValue(p);
         else res[1]->getMesh()->getPositions()->addValue(p);
     }
 
@@ -274,19 +204,29 @@ vector<VRGeometry*> extractPlanes(VRGeometry* geo_in, int N, float delta) {
     srand(5);
 
     vector<VRGeometry*> res, tmp;
+
+    /*Accumulator_grid accu( 10*delta, 10*delta );
+    accu.pushGeometry(geo_in);
+    vector<VRPlane> planes = accu.eval();
+    cout << "found " << planes.size() << " planes" << endl;
+    for (int i=0; i<N; i++) {
+        tmp = extractPointsOnPlane(geo_in, planes[i], 0.02);
+        res.push_back( tmp[0] );
+    }*/
+
+
     for (int i=0; i<N; i++) {
         int D = 10 + 2*i;
         D *= delta;
-        Accumulator_octree accu( D, D );
-        //cout << " accumulator size: " << accu.size() << endl;
+        Accumulator_grid accu( D, D );
+        cout << " accumulator size: " << accu.size() << endl;
 
         accu.pushGeometry(geo_in);
 
-        vector<Vec4f> planes = evalAccumulatorOctree(accu);
+        vector<VRPlane> planes = accu.eval();
         cout << " planes" << planes.size() << endl;
-        tmp = extractPointsOnPlaneOctree(geo_in, planes[0], accu, 0.02);
+        tmp = extractPointsOnPlane(geo_in, planes[0], 0.02);
         res.push_back(tmp[0]);
-        cout << "  push to res " << tmp[0] << " and rest " << tmp[1] <<endl;
         geo_in = tmp[1];
     }
 
