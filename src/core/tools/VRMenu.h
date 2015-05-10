4a4aa69930dd2f0a99e8865e5ab2d6b90477365b
Victor <victor.haefner@gmail.com>
2015-05-10 23:40:13 +0200

    started the VRMenu tool

---
+++ src/core/tools/VRMenu.h
@@ -0,0 +1,37 @@
+#ifndef VRMENU_H_INCLUDED
+#define VRMENU_H_INCLUDED
+
+#include "core/objects/geometry/VRGeometry.h"
+#include "core/utils/VRFunction.h"
+
+OSG_BEGIN_NAMESPACE;
+using namespace std;
+
+class VRMenu : public VRGeometry {
+    private:
+        enum TYPE {
+            SPRITE,
+        };
+
+        TYPE mtype = SPRITE;
+        string group;
+
+        VRMenu* active;
+        VRMenu* selected;
+
+        VRFunction<int>* fkt;
+
+    public:
+        VRMenu(string path);
+
+        VRMenu* append(string path);
+        VRMenu* getActive();
+        VRMenu* getSelected();
+
+        void enter();
+        void move(int dir);
+};
+
+OSG_END_NAMESPACE;
+
+#endif // VRMENU_H_INCLUDED
