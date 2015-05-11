363b70e49586071777950a5435661e7c3801fca7
Filip <filip.szeliga@student.kit.edu>
2015-05-11 17:54:32 +0200

    Euler angles added to VRTransform, first results with COLLADA animations

--- src/core/scene/import/VRCOLLADA.cpp
+++ src/core/scene/import/VRCOLLADA.cpp
@@ -91,6 +91,7 @@ AnimationLibrary parseColladaAnimations(string data) {
         node = node->first_node("library_animations");
         if (node) { //found animations tag...look for animations
             for (xNode* animxNode : getxNodes(node, "animation")) {
+                cout << "going through animation tag..."<<endl<<endl;
                 Attrib* animID = animxNode->first_attribute("id");
                 xNode* channels = animxNode->first_node("channel");
                 xNode* sampler = animxNode->first_node("sampler");
@@ -122,32 +123,25 @@ AnimationLibrary parseColladaAnimations(string data) {
                     string samplerSemantic = samplerIn->first_attribute("semantic")->value();
                     string samplerSource = samplerIn->first_attribute("source")->value();
                     samplerSource = samplerSource.substr(1, samplerSource.size()-1);
-
                     anim.sampler.inputs[samplerSemantic] = samplerSource;
 
-                    for (xNode* animSource : getxNodes(animxNode, "source")) {
+                    for (xNode* animSource : getxNodes(animxNode, "source")) {//parse through all <source> nodes
+                        if (samplerSource.find(animSource->first_attribute("id")->value()) == string::npos) continue;
+
                         Source source;
                         source.ID = animSource->first_attribute("id")->value();
 
-                        if (samplerSource.find(source.ID) != string::npos) {
-                            //now we have the semantics (sampler node) INPUT,OUTPUT,INTERPOLATION,INTANGENT and OUTTANGET to parse through...
-                            for (xNode* sourcexNode : getxNodes(animSource)) {
-                                string sourceName = sourcexNode->name();
-                                if (sourceName.find("_array") != string::npos) { // parse array
-                                    source.array_element = sourcexNode;
-                                } else if(sourceName == "technique_common") {
-                                    source.technique_common = sourcexNode;
-                                }
+                        for (xNode* sourcexNode : getxNodes(animSource)) {//parse through child nodes of source node
+                            string sourceName = sourcexNode->name();
+                            if (sourceName.find("_array") != string::npos) {
+                                source.array_element = sourcexNode;
+                            } else if(sourceName == "technique_common") {
+                                source.technique_common = sourcexNode;
                             }
-                            break;
                         }
-
                         anim.sources[source.ID] = source;
                     }
-
-                    //anim.sampler.inputs.push_back(sampIn);
                 }
-
                 library.animations[anim.ID] = anim;
             }
         } else cout << "<library_animations> tag not found" << endl;
@@ -175,18 +169,38 @@ void printAll(const AnimationLibrary& library) {
 
         cout << "      Sources:\n";
         for (auto s : a.second.sources){
-            cout << "         Source " << s.second.ID << endl;//prints source id
+            cout << "         Source " << s.second.ID << endl;//prints source i
             cout << "            Source technique " << s.second.technique_common << endl;//prints source id
-            cout << "            Source array " << s.second.array_element << endl;//prints source id
+            cout << "            Source array " << s.second.array_element->value() << endl;//prints source id
         }
     }
 }
 
-void setPose(OSG::VRTransform* o, int i, float t) {
+void setPose(OSG::VRTransform* o, int i, float t) {// object, axis, new axis values
     cout << "setPose " << o->getName() << " " << t << endl;
     Vec3f f = o->getFrom();
     f[i] = t;
     o->setFrom(f);
+}
+
+void setRot(OSG::VRTransform* o, int i, float t) {
+    cout << "setRot " << o->getName() << " " << t << endl;
+    Vec3f f = o->getEuler();
+    cout << "A"<< endl;
+
+    f[i] = t;
+        cout << "C"<< endl;
+
+    o->setEuler(f);
+        cout << "B"<< endl;
+
+}
+
+void setScale(OSG::VRTransform* o, int i, float t) {
+    cout << "setScale " << o->getName() << " " << t << endl;
+    Vec3f f = o->getScale();
+    f[i] = t;
+    o->setScale(f);
 }
 
 void setPose3(VRTransform* o, int i, Vec3f t) {
@@ -206,114 +220,114 @@ VRObject* findTarget(VRObject* o, string Name) {
     return 0;
 }
 
+template<class T>
+void string2Vector(char* input, vector<T> &output){
+        std::istringstream iss(input);
+        std::copy(std::istream_iterator<T>(iss),
+        std::istream_iterator<T>(),
+        std::back_inserter(output));
+}
+
 void buildAnimations(AnimationLibrary& lib, VRObject* objects) {
     for (auto a : lib.animations) {
         VRObject* obj = findTarget(objects, a.second.channel.target);
         if (obj == 0) continue;
         if (!obj->hasAttachment("transform")) continue;
 
-        float duration = 0;
-        float offset = 0.0;
-        bool loop = false;
-        int i = 0; // x axis
-
         map<string, Source> sources = a.second.sources;
         Sampler sampler = a.second.sampler;
         Source inputSource = sources.find(sampler.inputs.find("INPUT")->second)->second;
-        cout << "starting animation build for source: " << inputSource.ID << endl;
+        Source outputSource = sources.find(sampler.inputs.find("OUTPUT")->second)->second;
+        Source interpolationSource = sources.find(sampler.inputs.find("INTERPOLATION")->second)->second;
+        Source intangentSource = sources.find(sampler.inputs.find("IN_TANGENT")->second)->second;
+        Source outtangentSource = sources.find(sampler.inputs.find("OUT_TANGENT")->second)->second;
 
+        vector<float> inputValues;
+        vector<float> outputValues;
+        vector<string> interpolationValues;
+        vector<float> intangentValues;
+        vector<float> outtangentValues;
+        string2Vector(inputSource.array_element->value(), inputValues);
+        string2Vector(outputSource.array_element->value(), outputValues);
+        string2Vector(interpolationSource.array_element->value(), interpolationValues);
+        string2Vector(intangentSource.array_element->value(), intangentValues);
+        string2Vector(outtangentSource.array_element->value(), outtangentValues);
+
+/*
+ *         xNode* technique_common = inputSource.technique_common;
+ *         xNode* accessor = technique_common->first_node("accessor");
+ *         string inputArrayType =  accessor->first_node("param")->first_attribute("type")->value();//differentiate type
+ *         string outputArrayType =  accessor->first_node("param")->first_attribute("type")->value();//differentiate type
+ *         string interpolationArrayType =  accessor->first_node("param")->first_attribute("type")->value();//differentiate type
+ *         string intangentArrayType =  accessor->first_node("param")->first_attribute("type")->value();//differentiate type
+ *         string outtangentArrayType =  accessor->first_node("param")->first_attribute("type")->value();//differentiate type
+ */
 
         xNode* technique_common = inputSource.technique_common;
         xNode* accessor = technique_common->first_node("accessor");
-
         int inputCount = toInt(accessor->first_attribute("count")->value());
+        int outputCount = toInt(accessor->first_attribute("count")->value());
+        int interpolationCount = toInt(accessor->first_attribute("count")->value());
+        int intangenCount = toInt(accessor->first_attribute("count")->value());
+        int outtangentCount = toInt(accessor->first_attribute("count")->value());
+
         int inputStride = toInt(accessor->first_attribute("stride")->value());
-        string arrayType =  accessor->first_node("param")->first_attribute("type")->value();//differentiate type
+        int outputStride = toInt(accessor->first_attribute("stride")->value());
+        int interpolationStride = toInt(accessor->first_attribute("stride")->value());
+        int intangentStride = toInt(accessor->first_attribute("stride")->value());
+        int outtangentStride = toInt(accessor->first_attribute("stride")->value());
 
-        vector<float> inputValues;
-        string arrayElemCount = inputSource.array_element->first_attribute("count")->value();
+        int inputArrayCount = toInt(inputSource.array_element->first_attribute("count")->value());
 
-
-
-
-  // If possible, always prefer std::vector to naked array
-
-  // Build an istream that holds the input string
-        std::istringstream iss(inputSource.array_element->value());
-        cout << "values to be parsed: " << inputSource.array_element->value() << endl;
-
-  // Iterate over the istream, using >> to grab floats
-  // and push_back to store them in the vector
-        std::copy(std::istream_iterator<float>(iss),
-        std::istream_iterator<float>(),
-        std::back_inserter(inputValues));
-
-  // Put the result on standard out
         cout << "input values: ";
         for(float i : inputValues) cout << i <<" ";
-
+        for(string i : interpolationValues) cout << i <<" ";
         cout << "accessor count " << inputCount << endl;
         cout << "accessor stride " << inputStride << endl;
-        cout << "arrayType " << arrayType << endl;
+       // cout << "arrayType " << inputArrayType << endl;
 
+        VRTransform* t = (VRTransform*)obj;
+        int axis = -1;
+        VRFunction<float>* fkt;
 
+        if(a.second.channel.type.find("rotation") != string::npos) {
+            if(a.second.channel.target.find("X") != string::npos) axis = 0;
+            else if(a.second.channel.target.find("Y") != string::npos) axis = 1;
+            else if(a.second.channel.target.find("Z") != string::npos) axis = 2;
+            cout << "found rotation animation " << endl;
+            fkt = new VRFunction<float>(a.first, boost::bind(setRot, t, axis, _1) );
 
+        } else {
+            if(a.second.channel.axis.find("X") != string::npos) axis = 0;
+            else if(a.second.channel.axis.find("Y") != string::npos) axis = 1;
+            else if(a.second.channel.axis.find("Z") != string::npos) axis = 2;
 
-/*
-        for(auto in : a.second.sampler.inputs){
-            cout << "***********buildAnimations, in.source" << in.second << endl;
-            auto source = sources.find(in.second)->second;
-            cout << "***********buildAnimations, source name" <<source.ID << endl;
-
-            xNode* accessor = source.technique_common->first_node("accessor");
-
-
-
-
-            if(in.semantic == "INPUT") {
-                cout << "***INPUT: "  << endl;
-               // cout << "***INPUT: " << input.ID << endl;
-
-               //my source is the input, so time
+            if(a.second.channel.type.find("location") != string::npos) {
+                cout << "found location animation " << endl;
+                fkt = new VRFunction<float>(a.first, boost::bind(setPose, t, axis, _1) );
+                //(*fkt)(1.0)
             }
-            else if(in.semantic == "OUTPUT") {
-                cout << "***OUTPUT" << endl;
+            else if(a.second.channel.type.find("scale") != string::npos){
+                 cout << "found scale animation " << endl;
+                fkt = new VRFunction<float>(a.first, boost::bind(setScale, t, axis, _1) );
             }
-            else if(in.semantic == "INTERPOLATION") {
-                cout << "***INTERPOLATION" << endl;
-            }
-            else if(in.semantic == "IN_TANGENT") {
-                cout << "***INTANGENT" << endl;
-            }
-            else if(in.semantic == "OUT_TANGENT") {
-                cout << "***OUTTANGENT" << endl;
-            }
-
-
-            cout << "accessor count " << counts << endl;
-            cout << "accessor stride " << stride << endl;
 
         }
 
 
-        */
-        VRTransform* t = (VRTransform*)obj;
-
-        //float duration = 2.0;
-        //float offset = 0.0;
-        //bool loop = false;
-
-        //int i = 0; // x axis
-        auto fkt = new VRFunction<float>(a.first, boost::bind(setPose, t, i, _1) );
-        float start = 0.0;
-        float end = 1.0;
-
-        //auto fkt = new VRFunction<Vec3f>(a.first, boost::bind(setPose, t, i, _1) );
+        //auto fkt = new VRFunction<Vec3f>(a.first, boost::bind(setPose3f, t, i, _1) );
         //Vec3f start = Vec3f(x,y,z);
         //Vec3f end = Vec3f(2*x,y,z);
 
-        VRAnimation* anim = new VRAnimation(duration, offset, fkt, start, end, loop);
-        t->addAnimation(anim);
+        for(size_t i = 0; i < inputArrayCount - 1; i++){
+            float start = outputValues[i];
+            float end = outputValues[i+1];
+            float duration = inputValues[i+1] - inputValues[i];
+            float offset = inputValues[i];
+            bool loop = false;
+            VRAnimation* anim = new VRAnimation(duration, offset, fkt, start, end, loop);
+            t->addAnimation(anim);
+        }
     }
 }
 
