a3c74242bfcb452de808e793bc736a6a932b4665
Victor <victor.haefner@gmail.com>
2015-05-26 23:24:04 +0200

    py menu documentation

--- src/core/scripting/VRPyMenu.cpp
+++ src/core/scripting/VRPyMenu.cpp
@@ -49,8 +49,8 @@ template<> PyTypeObject VRPyBaseT<OSG::VRMenu>::type = {
 
 PyMethodDef VRPyMenu::methods[] = {
     {"append", (PyCFunction)VRPyMenu::append, METH_VARARGS, "Append a child menu - append(str texture_path)" },
-    {"setLeafType", (PyCFunction)VRPyMenu::setLeafType, METH_VARARGS, "Set menu layout - setLeafType(str type, float scale)" },
-    {"setLayout", (PyCFunction)VRPyMenu::setLayout, METH_VARARGS, "Set menu layout - setLayout(str layout, float param)" },
+    {"setLeafType", (PyCFunction)VRPyMenu::setLeafType, METH_VARARGS, "Set menu layout - setLeafType(str type, vec2f scale)\n type : ['SPRITE'], scale is the size of the sprite" },
+    {"setLayout", (PyCFunction)VRPyMenu::setLayout, METH_VARARGS, "Set menu layout - setLayout(str layout, float param)\n layout : ['LINEAR', 'CIRCULAR'], param is the distance between leafs" },
     {"open", (PyCFunction)VRPyMenu::open, METH_NOARGS, "Open menu" },
     {"close", (PyCFunction)VRPyMenu::close, METH_NOARGS, "Close menu" },
     {"setCallback", (PyCFunction)VRPyMenu::setCallback, METH_VARARGS, "Set a menu callback - setCallback(fkt, [params])" },
