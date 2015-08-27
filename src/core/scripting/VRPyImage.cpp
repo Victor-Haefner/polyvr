59367278da66c1e3b70d0eb620d7dc8a052ade2e
Jorge Mario Nieto <jorge.martinez@kit.edu>
2015-08-27 16:30:41 +0200

    fixed memleak in VRPyImage

--- src/core/scripting/VRPyImage.cpp
+++ src/core/scripting/VRPyImage.cpp
@@ -12,7 +12,7 @@ template<> PyTypeObject VRPyBaseT<OSG::Image>::type = {
     "VR.Image",             /*tp_name*/
     sizeof(VRPyImage),             /*tp_basicsize*/
     0,                         /*tp_itemsize*/
-    (destructor)dealloc, /*tp_dealloc*/
+    (destructor)VRPyImage::dealloc, /*tp_dealloc*/
     0,                         /*tp_print*/
     0,                         /*tp_getattr*/
     0,                         /*tp_setattr*/
@@ -45,24 +45,23 @@ template<> PyTypeObject VRPyBaseT<OSG::Image>::type = {
     0,                         /* tp_dictoffset */
     (initproc)init,      /* tp_init */
     0,                         /* tp_alloc */
-    VRPyImage::NewImg,                 /* tp_new */
+    VRPyImage::New,                 /* tp_new */
 };
 
 PyMethodDef VRPyImage::methods[] = {
     {NULL}  /* Sentinel */
 };
 
-PyObject* VRPyImage::NewImg(PyTypeObject *type, PyObject *args, PyObject *kwds) {
+PyObject* VRPyImage::New(PyTypeObject *type, PyObject *args, PyObject *kwds) {
     OSG::ImageRecPtr img = OSG::Image::create();
-    if (pySize(args) == 3) {
-        import_array1(NULL);
-        PyArrayObject* data = 0;
-        int W, H;
-        if (! PyArg_ParseTuple(args, "Oii", &data, &W, &H)) return NULL;
-        if ((PyObject*)data == Py_None) Py_RETURN_TRUE;
-        unsigned char* cdata  = (unsigned char*)PyArray_DATA(data);
-        img->set(OSG::Image::OSG_RGB_PF, W, H, 1, 1, 1, 0, cdata, OSG::Image::OSG_UINT8_IMAGEDATA, true);
-    } else cout << "VRPyImage::NewImg - pass data, width and height" << endl;
+
+    import_array1(NULL);
+    PyArrayObject* data = 0;
+    int W, H;
+    if (! PyArg_ParseTuple(args, "Oii", &data, &W, &H)) return NULL;
+    if ((PyObject*)data == Py_None) Py_RETURN_TRUE;
+    unsigned char* cdata  = (unsigned char*)PyArray_DATA(data);
+    img->set(OSG::Image::OSG_RGB_PF, W, H, 1, 1, 1, 0, cdata, OSG::Image::OSG_UINT8_IMAGEDATA, true);
 
     VRPyImage* pyimg = (VRPyImage*)alloc( type, img );
     pyimg->img = img;
@@ -70,4 +69,8 @@ PyObject* VRPyImage::NewImg(PyTypeObject *type, PyObject *args, PyObject *kwds) 
     return (PyObject*)pyimg;
 }
 
+void VRPyImage::dealloc(VRPyImage* self) {
+    if (self->img != 0) self->img = 0;
+    self->ob_type->tp_free((PyObject*)self);
+}
 
--- src/core/scripting/VRPyImage.h
+++ src/core/scripting/VRPyImage.h
@@ -8,7 +8,8 @@ struct VRPyImage : VRPyBaseT<OSG::Image> {
     static PyMethodDef methods[];
 
     OSG::ImageRecPtr img;
-    static PyObject* NewImg(PyTypeObject *type, PyObject *args, PyObject *kwds);
+    static PyObject* New(PyTypeObject *type, PyObject *args, PyObject *kwds);
+    static void dealloc(VRPyImage* self);
 };
 
 #endif // VRPYIMAGE_H_INCLUDED
