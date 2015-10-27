cc95e308caf7c11c59f0f7001b6c6349b67daf9f
Victor <victor.haefner@gmail.com>
2015-10-27 21:47:43 +0100

    enabled start script to be called from another location

--- PolyVR.sh
+++ PolyVR.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 #export LD_PRELOAD=/home/victor/Projects/polyvr/src/debugging/exception_preload.so
 libs=/usr/lib/opensg:/usr/lib/CEF:/usr/lib/1.4:/usr/lib/virtuose
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 
 export LD_LIBRARY_PATH=$libs && ./bin/Debug/VRFramework $@
 #export LD_LIBRARY_PATH=$libs && gdb bin/Debug/VRFramework
