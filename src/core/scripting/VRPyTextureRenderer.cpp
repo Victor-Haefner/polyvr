586ee2d66d097b5b114080073fe17277ecb8873a
Victor <victor.haefner@gmail.com>
2016-02-04 19:33:53 +0100

    moved link py bindings

--- src/core/scripting/VRPyTextureRenderer.cpp
+++ src/core/scripting/VRPyTextureRenderer.cpp
@@ -10,26 +10,8 @@ simpleVRPyType(TextureRenderer, New_VRObjects_ptr);
 PyMethodDef VRPyTextureRenderer::methods[] = {
     {"setup", (PyCFunction)VRPyTextureRenderer::setup, METH_VARARGS, "Setup texture renderer - setup( cam, width, height)" },
     {"getMaterial", (PyCFunction)VRPyTextureRenderer::getMaterial, METH_NOARGS, "Get the material with the rendering - getMaterial()" },
-    {"addLink", (PyCFunction)VRPyTextureRenderer::addLink, METH_VARARGS, "Link subtree - addLink( object )" },
-    {"remLink", (PyCFunction)VRPyTextureRenderer::remLink, METH_VARARGS, "Unlink subtree - remLink( object )" },
     {NULL}  /* Sentinel */
 };
-
-PyObject* VRPyTextureRenderer::addLink(VRPyTextureRenderer* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-    VRPyObject* o;
-    if (!PyArg_ParseTuple(args, "O", &o)) return NULL;
-    self->objPtr->addLink( o->objPtr );
-    Py_RETURN_TRUE;
-}
-
-PyObject* VRPyTextureRenderer::remLink(VRPyTextureRenderer* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-    VRPyObject* o;
-    if (!PyArg_ParseTuple(args, "O", &o)) return NULL;
-    self->objPtr->remLink( o->objPtr );
-    Py_RETURN_TRUE;
-}
 
 PyObject* VRPyTextureRenderer::setup(VRPyTextureRenderer* self, PyObject* args) {
     if (!self->valid()) return NULL;
