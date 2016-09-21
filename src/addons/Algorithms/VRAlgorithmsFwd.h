d190823347c6ce701d3560e0429313fc795eee0b
Victor <victor.haefner@gmail.com>
2016-09-21 17:44:48 +0200

    added algorithms fwd delc header

---
+++ src/addons/Algorithms/VRAlgorithmsFwd.h
@@ -0,0 +1,10 @@
+#ifndef VRALGORITHMSFWD_H_INCLUDED
+#define VRALGORITHMSFWD_H_INCLUDED
+
+#include "core/utils/VRFwdDeclTemplate.h"
+
+namespace OSG {
+    ptrFwd(VRGraphLayout);
+}
+
+#endif // VRALGORITHMSFWD_H_INCLUDED
