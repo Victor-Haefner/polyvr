d65a845e7f8f38431fbb16b5893b080e1d54f9f3
Victor <victor.haefner@gmail.com>
2016-11-16 22:19:45 +0100

    fixed bugs in VRTexture getPixel

--- src/core/objects/material/VRTexture.cpp
+++ src/core/objects/material/VRTexture.cpp
@@ -21,10 +21,82 @@ ImageRecPtr VRTexture::getImage() { return img; }
 void VRTexture::read(string path) { img->read(path.c_str()); }
 void VRTexture::write(string path) { img->write(path.c_str()); }
 
-Vec4f VRTexture::getPixel(Vec2f uv) {
+int VRTexture::getChannels() {
+    if (!img) return 0;
+    auto f = img->getPixelFormat();
+    if (f == Image::OSG_A_PF) return 1;
+    if (f == Image::OSG_I_PF) return 1;
+    if (f == Image::OSG_L_PF) return 1;
+    if (f == Image::OSG_DEPTH_PF) return 1;
+    if (f == Image::OSG_ALPHA_INTEGER_PF) return 1;
+    if (f == Image::OSG_LUMINANCE_INTEGER_PF) return 1;
+    if (f == Image::OSG_DEPTH_PF) return 1;
+    if (f == Image::OSG_DEPTH_PF) return 1;
+    if (f == Image::OSG_LA_PF) return 2;
+    if (f == Image::OSG_LUMINANCE_ALPHA_INTEGER_PF) return 2;
+    if (f == Image::OSG_RGB_PF) return 3;
+    if (f == Image::OSG_RGB_INTEGER_PF) return 3;
+    if (f == Image::OSG_BGR_INTEGER_PF) return 3;
+    if (f == Image::OSG_RGBA_PF) return 4;
+    if (f == Image::OSG_RGBA_INTEGER_PF) return 4;
+    if (f == Image::OSG_BGRA_INTEGER_PF) return 4;
+    return 0;
+}
+
+/*int VRTexture::getByteSize() {
+    if (!img) return 0;
+    auto f = img->getPixelFormat();
+    Image::
+    if (OSG_INVALID_IMAGEDATATYPE  = GL_NONE,
+        OSG_UINT8_IMAGEDATA        = GL_UNSIGNED_BYTE,
+        OSG_UINT16_IMAGEDATA       = GL_UNSIGNED_SHORT,
+        OSG_UINT32_IMAGEDATA       = GL_UNSIGNED_INT,
+        OSG_FLOAT16_IMAGEDATA      = GL_HALF_FLOAT_NV,
+        OSG_FLOAT32_IMAGEDATA      = GL_FLOAT,
+        OSG_INT16_IMAGEDATA        = GL_SHORT,
+        OSG_INT32_IMAGEDATA        = GL_INT,
+        OSG_UINT24_8_IMAGEDATA     = GL_NONE
+    return 0;
+}*/
+
+Vec4f VRTexture::getPixel(Vec2f uv) { // TODO: check data format (float/integer/char)
     auto res = Vec4f(0,0,0,1);
     if (!img) return res;
+    int N = getChannels();
+    int B = img->getBpp()/N;
+    int w = img->getWidth();
+    int h = img->getHeight();
+    int x = uv[0]*(w-1);
+    int y = uv[1]*(h-1);
     auto data = img->getData();
-    //return data[]; // TODO
+    int i = x + y*w;
+
+    if (N == 1) {
+        float* f = (float*)data;
+        float d = f[i];
+        res = Vec4f(d, d, d, 1.0);
+    }
+
+    if (N == 2) {
+        Vec2f* data2 = (Vec2f*)data;
+        Vec2f d = data2[i];
+        res = Vec4f(d[0], d[1], 0.0, 1.0);
+    }
+
+    if (N == 3) {
+        Vec3f* data3 = (Vec3f*)data;
+        Vec3f d = data3[i];
+        res = Vec4f(d[0], d[1], d[2], 1.0);
+    }
+
+    if (N == 4) {
+        Vec4f* data4 = (Vec4f*)data;
+        res = data4[i];
+    }
+
+    //cout << "VRTexture::getPixel " << Vec4i(B, N, w, h) << " uv " << uv << " i " << i << " xy " << Vec2i(x,y) <<  " N " << w*h << " c " << res << endl;
+    //cout << "  size " << img->getSize();
     return res;
 }
+
+
--- src/core/objects/material/VRTexture.h
+++ src/core/objects/material/VRTexture.h
@@ -33,6 +33,7 @@ class VRTexture : public std::enable_shared_from_this<VRTexture> {
 
         void read(string path);
         void write(string path);
+        int getChannels();
         Vec4f getPixel(Vec2f uv);
 };
 
