3326c4f80742b60fc052a7ce47460e44bd0b7ef0
Victor Haefner <victor.haefner@gmail.com>
2017-09-11 22:07:06 +0200

    working on terrain physics shape

--- src/addons/WorldGenerator/terrain/VRTerrainPhysicsShape.cpp
+++ src/addons/WorldGenerator/terrain/VRTerrainPhysicsShape.cpp
@@ -9,52 +9,47 @@
 using namespace OSG;
 
 VRTerrainPhysicsShape::VRTerrainPhysicsShape(VRTerrainPtr terrain) : terrain(terrain) {
-    auto tex = terrain->getMap();
-    auto dim = tex->getSize();
-
-    double Hmax = -1e6;
-    for (int i = 0; i < dim[0]; i++) {
-        for (int j = 0; j < dim[1]; j++) {
-            float h = tex->getPixel(Vec3i(i,j,0))[3];
-            if (Hmax < h) Hmax = h;
-        }
-    }
-
-    initialize(dim[0], dim[1], 1, -Hmax, Hmax);
+    initialize();
     auto texelSize = terrain->getTexelSize();
     setLocalScaling(btVector3(texelSize[0],1,texelSize[1]));
 }
 
 VRTerrainPhysicsShape::~VRTerrainPhysicsShape() {;}
 
-void VRTerrainPhysicsShape::initialize (int heightStickWidth, int heightStickLength, btScalar heightScale, btScalar minHeight, btScalar maxHeight) {
-	btAssert(heightStickWidth > 1 && "bad width");
-	btAssert(heightStickLength > 1 && "bad length");
-	btAssert(heightfieldData && "null heightfield data");
+void VRTerrainPhysicsShape::initialize () {
+    auto tex = terrain->getMap();
+    auto dim = tex->getSize();
 
 	// initialize member variables
 	m_shapeType = TERRAIN_SHAPE_PROXYTYPE;
-	m_heightStickWidth = heightStickWidth;
-	m_heightStickLength = heightStickLength;
-	m_minHeight = minHeight;
-	m_maxHeight = maxHeight;
-	m_width = (btScalar) (heightStickWidth - 1);
-	m_length = (btScalar) (heightStickLength - 1);
-	m_heightScale = heightScale;
+	m_heightStickWidth = dim[0];
+	m_heightStickLength = dim[1];
+	m_width = (btScalar) (dim[0] - 1);
+	m_length = (btScalar) (dim[1] - 1);
 	m_localScaling.setValue(btScalar(1.), btScalar(1.), btScalar(1.));
-    //m_localAabbMin.setValue(0, m_minHeight, 0);
-    //m_localAabbMax.setValue(m_width, m_maxHeight, m_length);
-    auto bb = terrain->getBoundingBox();
-    m_localAabbMin = VRPhysics::toBtVector3( bb.min() );
-    m_localAabbMax = VRPhysics::toBtVector3( bb.max() );
-	m_localOrigin = btVector3(0,0,0);
+    boundingbox = terrain->getBoundingBox();
+
+    Hmax = -1e6;
+    for (int i = 0; i < dim[0]; i++) {
+        for (int j = 0; j < dim[1]; j++) {
+            float h = tex->getPixel(Vec3i(i,j,0))[3];
+            if (Hmax < h) Hmax = h;
+        }
+    }
 }
 
+/*void VRTerrainPhysicsShape::getAabb(const btTransform& t, btVector3& aabbMin, btVector3& aabbMax) const {
+	Vec3d halfExtents = boundingbox.size() * 0.5;
+    auto p = pose( VRPhysics::fromBTTransform(t) );
+	aabbMin = VRPhysics::toBtVector3( p.transform(boundingbox.min()) );
+	aabbMax = VRPhysics::toBtVector3( p.transform(boundingbox.max()) );
+}*/
+
 void VRTerrainPhysicsShape::getAabb(const btTransform& t,btVector3& aabbMin,btVector3& aabbMax) const {
+    btVector3 m_localAabbMin = btVector3(0, -Hmax, 0);
+    btVector3 m_localAabbMax = btVector3(m_width, Hmax, m_length);
 	btVector3 halfExtents = (m_localAabbMax-m_localAabbMin)* m_localScaling * btScalar(0.5);
 	btVector3 localOrigin(0, 0, 0);
-	localOrigin[1] = (m_minHeight + m_maxHeight) * btScalar(0.5);
-	localOrigin *= m_localScaling;
 
 	btMatrix3x3 abs_b = t.getBasis().absolute();
 	btVector3 center = t.getOrigin();
@@ -65,18 +60,10 @@ void VRTerrainPhysicsShape::getAabb(const btTransform& t,btVector3& aabbMin,btVe
 	aabbMax = center + extent;
 }
 
-btScalar VRTerrainPhysicsShape::getRawHeightFieldValue(int x,int y) const {
-    return terrain->getMap()->getPixel(Vec3i(x,y,0))[3] + 0.03;
-}
-
 /// this returns the vertex in bullet-local coordinates
 void VRTerrainPhysicsShape::getVertex(int x,int y,btVector3& vertex) const {
-	btAssert(x>=0);
-	btAssert(y>=0);
-	btAssert(x<m_heightStickWidth);
-	btAssert(y<m_heightStickLength);
-	btScalar height = getRawHeightFieldValue(x,y);
-    vertex.setValue( (-m_width/btScalar(2.0)) + x, height - m_localOrigin.getY(), (-m_length/btScalar(2.0)) + y );
+	btScalar height = terrain->getMap()->getPixel(Vec3i(x,y,0))[3] + 0.03;
+    vertex.setValue( (-m_width/btScalar(2.0)) + x, height, (-m_length/btScalar(2.0)) + y );
 	vertex *= m_localScaling;
 }
 
@@ -92,8 +79,14 @@ static inline int getQuantized ( btScalar x ) {
   "with clamp" means that we restrict the point to be in the heightfield's
   axis-aligned bounding box.
  */
-void VRTerrainPhysicsShape::quantizeWithClamp(int* out, const btVector3& point,int /*isMax*/) const {
+void VRTerrainPhysicsShape::quantizeWithClamp(int* out, const btVector3& point) const {
+    btVector3 m_localAabbMin = btVector3(0, -Hmax, 0);
+    btVector3 m_localAabbMax = btVector3(m_width, Hmax, m_length);
+
 	btVector3 clampedPoint(point);
+	//clampedPoint.setMax( VRPhysics::toBtVector3(boundingbox.min()) );
+	//clampedPoint.setMin( VRPhysics::toBtVector3(boundingbox.max()) );
+
 	clampedPoint.setMax(m_localAabbMin);
 	clampedPoint.setMin(m_localAabbMax);
 
@@ -111,18 +104,14 @@ void VRTerrainPhysicsShape::quantizeWithClamp(int* out, const btVector3& point,i
  */
 void VRTerrainPhysicsShape::processAllTriangles(btTriangleCallback* callback,const btVector3& aabbMin,const btVector3& aabbMax) const {
 	// scale down the input aabb's so they are in local (non-scaled) coordinates
-	btVector3	localAabbMin = aabbMin*btVector3(1.f/m_localScaling[0],1.f/m_localScaling[1],1.f/m_localScaling[2]);
-	btVector3	localAabbMax = aabbMax*btVector3(1.f/m_localScaling[0],1.f/m_localScaling[1],1.f/m_localScaling[2]);
-
-	// account for local origin
-	localAabbMin += m_localOrigin;
-	localAabbMax += m_localOrigin;
+	btVector3 localAabbMin = aabbMin*btVector3(1.f/m_localScaling[0],1.f/m_localScaling[1],1.f/m_localScaling[2]);
+	btVector3 localAabbMax = aabbMax*btVector3(1.f/m_localScaling[0],1.f/m_localScaling[1],1.f/m_localScaling[2]);
 
 	//quantize the aabbMin and aabbMax, and adjust the start/end ranges
 	int	quantizedAabbMin[3];
 	int	quantizedAabbMax[3];
-	quantizeWithClamp(quantizedAabbMin, localAabbMin,0);
-	quantizeWithClamp(quantizedAabbMax, localAabbMax,1);
+	quantizeWithClamp(quantizedAabbMin, localAabbMin);
+	quantizeWithClamp(quantizedAabbMax, localAabbMax);
 
 	// expand the min/max quantized values
 	// this is to catch the case where the input aabb falls between grid points!
@@ -158,9 +147,11 @@ void VRTerrainPhysicsShape::processAllTriangles(btTriangleCallback* callback,con
 	}
 }
 
-void VRTerrainPhysicsShape::calculateLocalInertia(btScalar ,btVector3& inertia) const {
-    inertia.setValue(btScalar(0.),btScalar(0.),btScalar(0.)); //moving concave objects not supported
-}
-
+void VRTerrainPhysicsShape::calculateLocalInertia(btScalar ,btVector3& inertia) const { inertia.setValue(0,0,0); } //moving concave objects not supported
 void VRTerrainPhysicsShape::setLocalScaling(const btVector3& scaling) { m_localScaling = scaling; }
 const btVector3& VRTerrainPhysicsShape::getLocalScaling() const { return m_localScaling; }
+
+
+
+
+
--- src/addons/WorldGenerator/terrain/VRTerrainPhysicsShape.h
+++ src/addons/WorldGenerator/terrain/VRTerrainPhysicsShape.h
@@ -2,6 +2,7 @@
 #define VRTERRAINPHYSICSSHAPE_H_INCLUDED
 
 #include "../VRWorldGeneratorFwd.h"
+#include "core/math/boundingbox.h"
 #include <vector>
 #include <memory>
 #include <OpenSG/OSGConfig.h>
@@ -13,23 +14,17 @@ OSG_BEGIN_NAMESPACE;
 ATTRIBUTE_ALIGNED16(class)  VRTerrainPhysicsShape : public btConcaveShape {
     protected:
         VRTerrainPtr terrain;
-        btVector3 m_localAabbMin;
-        btVector3 m_localAabbMax;
-        btVector3 m_localOrigin;
-
+        Boundingbox boundingbox;
+        double Hmax;
         int	m_heightStickWidth;
         int m_heightStickLength;
-        btScalar m_minHeight;
-        btScalar m_maxHeight;
         btScalar m_width;
         btScalar m_length;
-        btScalar m_heightScale;
 
         btVector3 m_localScaling;
-        virtual btScalar getRawHeightFieldValue(int x,int y) const;
-        void quantizeWithClamp(int* out, const btVector3& point,int isMax) const;
+        void quantizeWithClamp(int* out, const btVector3& point) const;
         void getVertex(int x,int y,btVector3& vertex) const;
-        void initialize(int heightStickWidth, int heightStickLength, btScalar heightScale, btScalar minHeight, btScalar maxHeight);
+        void initialize();
 
     public:
         BT_DECLARE_ALIGNED_ALLOCATOR();
