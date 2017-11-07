9b466f8c32bd948d23b204cc8f4f08ce1e912b98
Your Name <you@example.com>
2017-11-07 10:55:55 +0100

    further virtuose refactoring

--- src/core/setup/devices/virtuose/main.cpp
+++ src/core/setup/devices/virtuose/main.cpp
@@ -192,6 +192,76 @@ class Device {
 
         bool attached = false;
         bool run = true;
+        bool shifting = true;
+
+        void updateVariables() {
+            interface.lock();
+            run = interface.getObject<bool>("run", false);
+            virtGetAvatarPosition(vc, position.data);
+            interface.setObject<Vec7>("position", position);
+
+            int _shifting = 0;
+            int power = 0;
+            virtGetPowerOn(vc, &power);
+            virtIsInShiftPosition(vc, &_shifting);
+            shifting = bool(_shifting || !power);
+            interface.setObject<bool>("shifting", shifting);
+            interface.unlock();
+        }
+
+        void applyForces() {
+            /*if (interface.hasObject<Vec6>("targetForces")) {
+                auto forces = interface.getObject<Vec6>("targetForces");
+                //print(forces, 6);
+                virtAddForce(vc, forces.data);
+            }*/
+        }
+
+        void updateAttached() {
+            //cout << "\nattached " << attached << " hasTargetSpeed " << interface.hasObject<Vec6>("targetSpeed") << " targetPosition " << interface.hasObject<Vec7>("targetPosition");
+            if (attached && interface.hasObject<Vec6>("targetSpeed") && interface.hasObject<Vec7>("targetPosition")) {
+                auto targetSpeed = interface.getObject<Vec6>("targetSpeed");
+                auto targetPosition = interface.getObject<Vec7>("targetPosition");
+                float tmpPos[7];
+                virtGetPosition(vc, tmpPos);
+                for(int i = 0; i < 7 ; i++) tmpPos[i] = targetPosition.data[i] - tmpPos[i];
+                virtSetPosition(vc, targetPosition.data);
+
+                float tmpSp[6];
+                virtGetSpeed(vc, tmpSp);
+                for(int i = 0; i < 6 ; i++) tmpSp[i] = targetSpeed.data[i] - tmpSp[i];
+                virtSetSpeed(vc, targetSpeed.data);
+
+                float Lp = sqrt( tmpPos[0]*tmpPos[0]+tmpPos[1]*tmpPos[1]+tmpPos[2]*tmpPos[2] );
+                float Ls = sqrt( tmpSp[0]*tmpSp[0]+tmpSp[1]*tmpSp[1]+tmpSp[2]*tmpSp[2] );
+                float Lt = sqrt( tmpSp[3]*tmpSp[3]+tmpSp[4]*tmpSp[4]+tmpSp[5]*tmpSp[5] );
+                bool doPhysUpdate = ( Lp < 0.1 && Ls < 0.5 && Lt < 0.5 );
+                interface.setObject<bool>("doPhysUpdate", doPhysUpdate);
+            }
+        }
+
+        void transmitForces() {
+            virtGetForce(vc, forces.data);
+            interface.setObject<Vec6>("forces", forces);
+            //cout << "device: "; print(forces, 6);
+        }
+
+        void handleCommands() {
+            //cout << "\n has doAttach " << interface.hasObject<bool>("doAttach") << endl;
+            if (interface.getObject<bool>("doAttach", false)) {
+                cout << " --- virtuose attach!! ---\n";
+                interface.setObject<bool>("doAttach", false);
+                auto mass = interface.getObject<float>("mass");
+                auto inertia = interface.getObject<Vec9>("inertia");
+                virtAttachVO(vc, mass, inertia.data);
+                attached = true;
+            }
+
+            //float position[7] = {0.0,0.0,0.0,0.0,0.0,0.0,1.0};
+            //float speed[6] = {0.0,0.0,0.0,0.0,0.0,0.0};
+            //virtSetPosition(vc, position);
+            //virtSetSpeed(vc, speed);
+        }
 
     public:
         Device() : interface("virtuose", true) {
@@ -225,67 +295,12 @@ class Device {
 
         void start() {
             do {
-                interface.lock();
-                run = interface.getObject<bool>("run", false);
-                virtGetAvatarPosition(vc, position.data);
-                interface.setObject<Vec7>("position", position);
-
-                int _shifting = 0;
-                int power = 0;
-                virtIsInShiftPosition(vc, &_shifting);
-                bool shifting = bool(_shifting || power);
-                virtGetPowerOn(vc, &power);
-                interface.setObject<bool>("shifting", shifting);
-                interface.unlock();
-
+                updateVariables();
                 if (!shifting) {
-
-                    /*if (interface.hasObject<Vec6>("targetForces")) {
-                        auto forces = interface.getObject<Vec6>("targetForces");
-                        //print(forces, 6);
-                        virtAddForce(vc, forces.data);
-                    }*/
-
-                    //cout << "\nattached " << attached << " hasTargetSpeed " << interface.hasObject<Vec6>("targetSpeed") << " targetPosition " << interface.hasObject<Vec7>("targetPosition");
-                    if (attached && interface.hasObject<Vec6>("targetSpeed") && interface.hasObject<Vec7>("targetPosition")) {
-                        auto targetSpeed = interface.getObject<Vec6>("targetSpeed");
-                        auto targetPosition = interface.getObject<Vec7>("targetPosition");
-                        float tmpPos[7];
-                        virtGetPosition(vc, tmpPos);
-                        for(int i = 0; i < 7 ; i++) tmpPos[i] = targetPosition.data[i] - tmpPos[i];
-                        virtSetPosition(vc, targetPosition.data);
-
-                        float tmpSp[6];
-                        virtGetSpeed(vc, tmpSp);
-                        for(int i = 0; i < 6 ; i++) tmpSp[i] = targetSpeed.data[i] - tmpSp[i];
-                        virtSetSpeed(vc, targetSpeed.data);
-
-                        float Lp = sqrt( tmpPos[0]*tmpPos[0]+tmpPos[1]*tmpPos[1]+tmpPos[2]*tmpPos[2] );
-                        float Ls = sqrt( tmpSp[0]*tmpSp[0]+tmpSp[1]*tmpSp[1]+tmpSp[2]*tmpSp[2] );
-                        float Lt = sqrt( tmpSp[3]*tmpSp[3]+tmpSp[4]*tmpSp[4]+tmpSp[5]*tmpSp[5] );
-                        bool doPhysUpdate = ( Lp < 0.1 && Ls < 0.5 && Lt < 0.5 );
-                        interface.setObject<bool>("doPhysUpdate", doPhysUpdate);
-                    }
-
-                    virtGetForce(vc, forces.data);
-                    interface.setObject<Vec6>("forces", forces);
-                    cout << "device: "; print(forces, 6);
-
-                    // cmds
-                    //cout << "\n has doAttach " << interface.hasObject<bool>("doAttach") << endl;
-                    if (interface.getObject<bool>("doAttach", false)) {
-                        cout << " --- virtuose attach!! ---\n";
-                        interface.setObject<bool>("doAttach", false);
-                        auto mass = interface.getObject<float>("mass");
-                        auto inertia = interface.getObject<Vec9>("inertia");
-                        virtAttachVO(vc, mass, inertia.data);
-                        attached = true;
-                    }
-
-                    //float position[7] = {0.0,0.0,0.0,0.0,0.0,0.0,1.0};
-                    //float speed[6] = {0.0,0.0,0.0,0.0,0.0,0.0};
-                    //virtSetPosition(vc, position);
-                    //virtSetSpeed(vc, speed);
+                    applyForces();
+                    updateAttached();
+                    transmitForces();
+                    handleCommands();
                 }
                 usleep(2000);
             } while (run);
