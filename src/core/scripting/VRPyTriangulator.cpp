9f9355dfc40495e9ab08c409aa68b682aa6b5692
Frieder Haizmann <frieder.haizmann@yahoo.de>
2018-06-14 15:39:03 +0200

    refactored triangulator bindings

--- src/core/scripting/VRPyTriangulator.cpp
+++ src/core/scripting/VRPyTriangulator.cpp
@@ -8,20 +8,7 @@ using namespace OSG;
 simplePyType(Triangulator, New_ptr);
 
 PyMethodDef VRPyTriangulator::methods[] = {
-    {"add", (PyCFunction)VRPyTriangulator::add, METH_VARARGS, "Add VRPolygon - add( VRPolygon )" },
-    {"compute", (PyCFunction)VRPyTriangulator::compute, METH_NOARGS, "Compute geometry - geo compute()" },
+    {"add", PyWrapOpt2(Triangulator, add, "Add polygon, bool outer", "1", void, VRPolygon, bool ) },
+    {"compute", PyWrap2(Triangulator, compute, "Compute geometry", VRGeometryPtr ) },
     {NULL}  /* Sentinel */
 };
-
-PyObject* VRPyTriangulator::add(VRPyTriangulator* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-    VRPyPolygon* p;
-    if (! PyArg_ParseTuple(args, "O", &p)) return NULL;
-    self->objPtr->add( *p->objPtr );
-    Py_RETURN_TRUE;
-}
-
-PyObject* VRPyTriangulator::compute(VRPyTriangulator* self) {
-    if (!self->valid()) return NULL;
-    return VRPyGeometry::fromSharedPtr( self->objPtr->compute() );
-}
