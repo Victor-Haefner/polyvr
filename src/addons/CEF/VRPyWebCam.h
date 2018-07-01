f2b7e44f8fcc2270acb593c3256a69c0e7c19b78
Victor Haefner <victor.haefner@gmail.com>
2018-07-01 21:55:11 +0200

    refactored WebCam py bindings

--- src/addons/CEF/VRPyWebCam.h
+++ src/addons/CEF/VRPyWebCam.h
@@ -6,8 +6,6 @@
 
 struct VRPyWebCam : VRPyBaseT<OSG::VRWebCam> {
     static PyMethodDef methods[];
-
-    static PyObject* connect(VRPyWebCam* self, PyObject* args);
 };
 
 #endif // VRPYWEBCAM_H_INCLUDED
