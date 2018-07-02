800ecac9849ad64c19a162ce50844cc9043b04f6
Victor Haefner <victor.haefner@gmail.com>
2018-07-02 23:31:40 +0200

    refactored numbering engine and py bindings

--- src/addons/Engineering/VRNumberingEngine.cpp
+++ src/addons/Engineering/VRNumberingEngine.cpp
@@ -46,25 +46,31 @@ bool VRNumberingEngine::checkUIn(int i, int grp) {
     return false;
 }
 
-void VRNumberingEngine::add(Vec3d p, int N, float f, int d, int grp) {
-    if (checkUIn(grp)) return;
-
-    for (int i=0; i<N; i++) {
-        data->pushVert(p, Vec3d(0,grp,0));
+void VRNumberingEngine::resize(int N) {
+    int pN = data->size();
+    if (N < pN) return;
+    for (int i=pN; i<N; i++) {
+        data->pushVert(Pnt3d(), Vec3d());
         data->pushPoint();
     }
-
     data->apply( ptr() );
 }
 
 void VRNumberingEngine::set(int i, Vec3d p, float f, int d, int grp) {
     if (checkUIn(i,grp)) return;
+    resize(i+1);
     float f1 = floor(f);
     float f2 = f-f1;
     string sf2 = toString(f2);
     if (sf2.size() > 2) sf2 = sf2.substr(2,sf2.size()-1);
     f2 = toFloat(sf2);
     data->setVert(i, p, Vec3d(f1,grp,f2));
+}
+
+int VRNumberingEngine::add(Vec3d p, float f, int d, int grp) {
+    int pN = data->size();
+    set(pN, p, f, d, grp);
+    return pN;
 }
 
 void VRNumberingEngine::setPrePost(int grp, string pre, string post) {
