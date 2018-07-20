50f53f2f08dfed3810a533c13565a242d87f6a85
Victor Haefner <victor.haefner@gmail.com>
2018-07-20 16:54:55 +0200

    fix for graph node removal

--- src/core/math/graph.h
+++ src/core/math/graph.h
@@ -69,6 +69,7 @@ class Graph : public VRStorage {
         int getEdgeID(int n1, int n2);
         vector< node >& getNodes();
         vector< node > getNodesCopy();
+        vector< node > getNeighbors(int i);
         vector< vector<edge> >& getEdges();
         vector< edge > getEdgesCopy();
         vector< edge > getInEdges(int n);
