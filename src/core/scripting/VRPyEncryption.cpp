211167919c257b7585631a0bf132480de699d12d
Victor Haefner <victor.haefner@gmail.com>
2018-07-22 21:07:07 +0200

    encryption module doc strings

--- src/core/scripting/VRPyEncryption.cpp
+++ src/core/scripting/VRPyEncryption.cpp
@@ -6,12 +6,12 @@ using namespace OSG;
 simpleVRPyType(Encryption, New_ptr);
 
 PyMethodDef VRPyEncryption::methods[] = {
-    {"encrypt", PyWrap( Encryption, encrypt, "Run test 1", string, string, string, string ) },
-    {"decrypt", PyWrap( Encryption, decrypt, "Run test 1", string, string, string, string ) },
-    {"compressFolder", PyWrap( Encryption, compressFolder, "Run test 1", void, string, string ) },
-    {"uncompressFolder", PyWrap( Encryption, uncompressFolder, "Run test 1", void, string, string ) },
-    {"loadAsString", PyWrap( Encryption, loadAsString, "Run test 1", string, string ) },
-    {"storeBin", PyWrap( Encryption, storeBin, "Run test 1", void, string, string ) },
+    {"encrypt", PyWrap( Encryption, encrypt, "Encrypt a string: plaintext, key, iv", string, string, string, string ) },
+    {"decrypt", PyWrap( Encryption, decrypt, "Decrypt a string: plaintext, key, iv", string, string, string, string ) },
+    {"compressFolder", PyWrap( Encryption, compressFolder, "Compress folder: folder, archive", void, string, string ) },
+    {"uncompressFolder", PyWrap( Encryption, uncompressFolder, "Uncompress folder: archive, folder", void, string, string ) },
+    {"loadAsString", PyWrap( Encryption, loadAsString, "Load file and return as string", string, string ) },
+    {"storeBin", PyWrap( Encryption, storeBin, "Store string as a binary file: data, path", void, string, string ) },
     {"test1", PyWrap( Encryption, test1, "Run test 1", void ) },
     {"test2", PyWrap( Encryption, test2, "Run test 2", void ) },
     {NULL}  /* Sentinel */
