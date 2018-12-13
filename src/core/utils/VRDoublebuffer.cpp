9cfcadc9289f0a5966f09fbc262af8e353503abd
Felix Michels <felix@dislin.de>
2018-12-13 15:43:21 +0100

    changed doublebuffer from reference to object

--- src/core/utils/VRDoublebuffer.cpp
+++ src/core/utils/VRDoublebuffer.cpp
@@ -4,33 +4,26 @@ OSG_BEGIN_NAMESPACE;
 using namespace std;
 
 doubleBuffer::doubleBuffer() {
-    t1 = new Matrix4d();
-    t2 = new Matrix4d();
-
     read1 = read2 = write2 = false;
     write1 = true;
-
     reading = writing = newest = false;
 }
 
-doubleBuffer::~doubleBuffer() {
-    delete t1;
-    delete t2;
-}
+doubleBuffer::~doubleBuffer() {}
 
 void doubleBuffer::read(Matrix4d& result) {
     reading = true;
     if(writing) {
         if (write1) {
             read2 = true;
-            result = *t2;
+            result = t2;
             read2 = false;
             reading = false;
             return;
         }
         if (write2) {
             read1 = true;
-            result = *t1;
+            result = t1;
             read1 = false;
             reading = false;
             return;
@@ -38,12 +31,12 @@ void doubleBuffer::read(Matrix4d& result) {
     } else {
         if (newest) {
             read2 = true;
-            result = *t2;
+            result = t2;
             read2 = false;
         }
         else {
             read1 = true;
-            result = *t1;
+            result = t1;
             read1 = false;
         }
         reading = false;
@@ -57,7 +50,7 @@ void doubleBuffer::write(Matrix4d m) {
     if(reading) {
         if (read1) {
             write2 = true;
-            *t2 = m;
+            t2 = m;
             newest = true;
             write2 = false;
             writing = false;
@@ -65,7 +58,7 @@ void doubleBuffer::write(Matrix4d m) {
         }
         if (read2) {
             write1 = true;
-            *t1 = m;
+            t1 = m;
             newest = false;
             write1 = false;
             writing = false;
@@ -74,11 +67,11 @@ void doubleBuffer::write(Matrix4d m) {
     } else {
         if (newest) {
             write1 = true;
-            *t1 = m;
+            t1 = m;
             write1 = false;
         } else {
             write2 = true;
-            *t2 = m;
+            t2 = m;
             write2 = false;
         }
         newest = !newest;
