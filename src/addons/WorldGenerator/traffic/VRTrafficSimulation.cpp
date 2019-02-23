ef8f551be7d7274465b48f798e85ec19090b49ab
Felix Michels <felix@dislin.de>
2019-02-23 23:39:26 +0800

    changed spawn radius minimum to 15m

--- src/addons/WorldGenerator/traffic/VRTrafficSimulation.cpp
+++ src/addons/WorldGenerator/traffic/VRTrafficSimulation.cpp
@@ -398,6 +398,7 @@ void VRTrafficSimulation::trafficSimThread(VRThreadWeakPtr tw) {
                 float D2 = (ep2-p).length();
 
                 if (D1 > userRadius && D2 > userRadius) continue; // outside
+                if (D1 < 15 || D2 < 15) continue;
                 if (debugOverRideSeedRoad<0 && graph->getPrevEdges(e).size() == 0  && !isPedestrian(e.ID) && !isParkingLane(e.ID) && !isIntersectionLane(e.ID)) { // roads that start out of "nowhere"
                     newSeedRoads.push_back( e.ID );
                     continue;
