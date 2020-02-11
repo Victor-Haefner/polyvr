edf3def0b3af90153df9d13205bdfdb8bb0d5560
Victor Haefner <victor.haefner@gmail.com>
2020-02-11 20:48:05 +0100

    working on ann eng

--- src/core/tools/VRText.cpp
+++ src/core/tools/VRText.cpp
@@ -16,11 +16,35 @@
 #include <pango/pangocairo.h>
 #endif
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+
 #include "core/objects/material/VRTexture.h"
 
 
 using namespace std;
 using namespace OSG;
+
+
+
+class FTUtils {
+    private:
+        FT_Library  library;
+        FT_Face     face;
+
+    public:
+        FTUtils() {
+            int e = FT_Init_FreeType( &library );
+            if (e) cout << "FT_Init_FreeType failed!" << endl;
+
+            e = FT_New_Face( library, "/usr/share/fonts/truetype/arial.ttf", 0, &face );
+            if (e) cout << "FT_New_Face failed!" << endl;
+        }
+};
+
+
+
 
 
 VRText* VRText::get() {
