544fcdd653337c8476c580e057f261c7d98e315a
Victor Haefner <victor.haefner@gmail.com>
2020-09-04 17:29:23 +0200

    fixed compilation

--- src/core/scene/import/VRDWG.cpp
+++ src/core/scene/import/VRDWG.cpp
@@ -474,7 +474,7 @@ void process_ARC(Dwg_Object* obj, DWGContext& data) {
 void process_TEXT(Dwg_Object* obj, DWGContext& data) {
     Dwg_Entity_TEXT* text = obj->tio.entity->tio.TEXT;
     Dwg_Object_LAYER* layer = getEntityLayer(obj, data);
-#if LIBREDWG_VERSION_MINOR >= 10
+#if LIBREDWG_VERSION_MINOR >= 11
     double x = text->ins_pt.x;
     double y = text->ins_pt.y;
 #else
