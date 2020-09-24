a79082b32ba22a72b67839c309282a6449abff1e
Victor Haefner <victor.haefner@gmail.com>
2020-09-24 18:23:24 +0200

    fixed windows getch

--- src/PolyVR.cpp
+++ src/PolyVR.cpp
@@ -530,17 +530,7 @@ char getch() {
 }
 #else
 TCHAR getch() {
-	DWORD mode, cc;
-	HANDLE h = GetStdHandle(STD_INPUT_HANDLE);
-	if (h == NULL) {
-		return 0; // console not found
-	}
-	GetConsoleMode(h, &mode);
-	SetConsoleMode(h, mode & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT));
-	TCHAR c = 0;
-	ReadConsole(h, &c, 1, &cc, NULL);
-	SetConsoleMode(h, mode);
-	return c;
+	return getchar();
 }
 #endif
 
