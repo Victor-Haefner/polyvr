4550846c9a589cb6300ea6c0736a391150145b9c
msi <msi@home.de>
2020-10-28 11:38:03 +0100

    compiling on D

--- start-win2.bat
+++ start-win2.bat
@@ -1,7 +1,9 @@
 @echo off
 
 set PATH=%PATH%;C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64;C:\usr\lib\opensg;C:\usr\vcpkg\installed\x64-windows\lib;C:\usr\lib\cef;
+set PATH=%PATH%;D:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64;D:\usr\lib\opensg;D:\usr\vcpkg\installed\x64-windows\lib;D:\usr\lib\cef;
 set PYTHONPATH=%PYTHONPATH%;C:\usr\vcpkg\installed\x64-windows\share\python2\Lib
+set PYTHONPATH=%PYTHONPATH%;D:\usr\vcpkg\installed\x64-windows\share\python2\Lib
 rem TODO: fix gschemas path!
 rem  set XDG_DATA_DIR=%XDG_DATA_DIR%;ressources\gui\schemas
 build\Release\polyvr.exe %*
