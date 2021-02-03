b4ae4d378d5005bcab25d06ff387226e33898589
Felix Michels <felix@dislin.de>
2021-02-03 20:04:07 +0100

    working on dynamic LOD

--- src/addons/WorldGenerator/GIS/VRAtlas.cpp
+++ src/addons/WorldGenerator/GIS/VRAtlas.cpp
@@ -14,6 +14,7 @@
 #include "core/objects/material/VRTexture.h"
 
 #include <iostream>
+#include <list>
 #include <fstream>
 #include <cmath>
 
@@ -27,9 +28,13 @@ VRAtlas::~VRAtlas() {}
 VRAtlasPtr VRAtlas::create() { return VRAtlasPtr( new VRAtlas() ); }
 //VRAtlasPtr VRAtlas::ptr() { return static_pointer_cast<VRAtlasPtr>(shared_from_this()); }
 
+VRAtlas::Patch::Patch(string sid, int lvl, VRTerrainPtr ter) : id(sid), LODlvl(lvl), terrain(ter) {}
+VRAtlas::Patch::Patch() {}
+VRAtlas::Patch::~Patch() {}
+
 void VRAtlas::test() { cout << "VRAtlas::test" << endl; }
 
-VRTerrainPtr VRAtlas::generateTerrain(string id, float edgeLength){
+VRTerrainPtr VRAtlas::generateTerrain(string id, int lvl , float edgeLength){
     string pathOrtho = "data/test64x64.jpg";
     string pathHeight = "data/testW64x64.jpg";
     string name = "testTer" + id;
@@ -40,7 +45,7 @@ VRTerrainPtr VRAtlas::generateTerrain(string id, float edgeLength){
     float mixAmount = 0;
     Vec2d terrainSize = Vec2d(edgeLength,edgeLength);
     //float scale = 64.0/edgeLength;
-    float scale = 1;
+    float scale = 1*float(pow(2,lvl));
 
     auto terrain = VRTerrain::create(name);
     terrain->setParameters (terrainSize, scale, 1);
@@ -79,8 +84,127 @@ VRGeometryPtr VRAtlas::generatePatch(string id) {
 void VRAtlas::update() {
     auto camDef = VRScene::getCurrent()->getActiveCamera();
     auto defCamPos = camDef->getWorldPosition();//camDef->getFrom();
+    int tmp = layout.currentLODlvl;
 
+    float dis = (defCamPos - layout.currentOrigin).length();
+    if (dis > 500.0 * pow(2.0, float(tmp))) upSize();
+    if (dis < 500.0 * ( pow(2.0, float(tmp) -1) ) && tmp > 0) downSize();
+    //cout << tmp << "---"  << dis << "---"  << 500.0 * pow(2.0, float(tmp)) << "---"  << 500.0 * ( pow(2.0, float(tmp)) -1) << "---" << endl;
+
+    if (layout.toDestroy.size() > 0){
+        for (auto &each : layout.toDestroy){
+            each.terrain->destroy();
+            patchcount--;
+        }
+        layout.toDestroy.clear();
+    }
     //cout << defCamPos << endl;
+}
+
+void VRAtlas::shiftLayout() {
+
+}
+
+void VRAtlas::downSize() {
+    layout.currentLODlvl--;
+    layout.currentMaxLODlvl--;
+    //destroy innerGrid
+    for (auto each : layout.innerQuad) layout.toDestroy.push_back(each);
+    for (auto each : layout.outerRing) layout.toDestroy.push_back(each);
+    layout.rings.pop_back(); //outerRing;
+    //make new innerGrid and new innerRing
+    addInnerQuad(layout.currentLODlvl);
+    addInnerRing(layout.currentLODlvl);
+    cout << "VRAtlas::downSize to lvl: " << layout.currentLODlvl << " -- "<< patchcount << endl;
+}
+
+void VRAtlas::upSize() {
+    layout.currentLODlvl++;
+    layout.currentMaxLODlvl++;
+    //destroy innerGrid
+    for (auto each : layout.innerQuad) layout.toDestroy.push_back(each);
+    for (auto each : layout.innerRing) layout.toDestroy.push_back(each);
+    layout.rings.pop_front(); //innerRing
+    layout.innerRing = layout.rings.front();
+    //make new innerGrid and outerRing
+    addInnerQuad(layout.currentLODlvl);
+    addOuterRing(layout.currentMaxLODlvl);
+    cout << "VRAtlas::upSize to lvl: " << layout.currentLODlvl << " iRs: "<< layout.innerRing.size() << " -- "<< patchcount <<  endl;
+}
+
+void VRAtlas::addInnerQuad(int lvl) {
+    layout.innerQuad.clear();
+    int fac = pow ( 2, lvl );
+    float nSize = size*fac;
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            string id = "LOD_" + toString(lvl) + "_" + toString(i) + toString(j);
+            auto ter = generateTerrain( id , lvl, nSize );
+            atlas->addChild(ter);
+            ter->setTransform(Vec3d((i-2)*nSize+nSize*0.5,0,(j-2)*nSize+nSize*0.5));
+            Patch p = Patch(id, lvl, ter);
+            layout.innerQuad.push_back(p);
+            patchcount++;
+        }
+    }
+    cout << 8*nSize << "m InnerQuad" << endl;
+}
+
+void VRAtlas::addInnerRing(int lvl) {
+    auto check = [&](int a, int b){
+        if (a < 2) return true;
+        if (b < 2) return true;
+        if (a > 5) return true;
+        if (b > 5) return true;
+        return false;
+    };
+    layout.innerRing.clear();
+    int fac = pow ( 2, lvl );
+    float nSize = size*fac;
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            if (check(i,j)){
+                string id = "LOD_" + toString(lvl) + "_" + toString(fac) + "_" + toString(i) + toString(j);
+                auto ter = generateTerrain( id , lvl, nSize );
+                atlas->addChild(ter);
+                ter->setTransform(Vec3d((i-4)*nSize+nSize*0.5,0,(j-4)*nSize+nSize*0.5));
+                Patch p = Patch(id, lvl, ter);
+                layout.innerRing.push_back(p);
+                patchcount++;
+            }
+        }
+    }
+    layout.rings.push_front(layout.innerRing);
+    cout << 8*nSize << "m InnerRing " << endl;
+}
+
+void VRAtlas::addOuterRing(int lvl) {
+    auto check = [&](int a, int b){
+        if (a < 2) return true;
+        if (b < 2) return true;
+        if (a > 5) return true;
+        if (b > 5) return true;
+        return false;
+    };
+
+    layout.outerRing.clear();
+    int fac = pow ( 2, lvl );
+    float nSize = size*fac;
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            if (check(i,j)){
+                string id = "LOD_" + toString(lvl) + "_" + toString(fac) + "_" + toString(i) + toString(j);
+                auto ter = generateTerrain( id , lvl, nSize );
+                atlas->addChild(ter);
+                ter->setTransform(Vec3d((i-4)*nSize+nSize*0.5,0,(j-4)*nSize+nSize*0.5));
+                Patch p = Patch(id, lvl, ter);
+                layout.outerRing.push_back(p);
+                patchcount++;
+            }
+        }
+    }
+    layout.rings.push_back(layout.outerRing);
+    cout << 8*nSize << "m OuterRing" << endl;
 }
 
 VRTransformPtr VRAtlas::setup() {
@@ -90,57 +214,23 @@ VRTransformPtr VRAtlas::setup() {
     VRScene::getCurrent()->addUpdateFkt(updatePtr);
 
     atlas = VRTransform::create("testTransform");
-    float size = 64.0;
-    float nSize;
-    int fac = 1;
-    string id = "";
 
     auto geo = generatePatch("000");
     atlas->addChild(geo);
 
-    auto check = [&](int a, int b){
-        if (a < 2) return true;
-        if (b < 2) return true;
-        if (a > 5) return true;
-        if (b > 5) return true;
-        return false;
-    };
+    addInnerQuad(0);
+    addInnerRing(0);
 
-    auto addRing = [&](){
-        //secondRing
-        fac = fac * 2;
-        nSize = size*fac;
-        for (int i = 0; i < 8; i++){
-            for (int j = 0; j < 8; j++){
-                if (check(i,j)){
-                    id = "_R" + toString(fac) + "_" + toString(i) + toString(j);
-                    auto ter = generateTerrain( id , nSize );
-                    atlas->addChild(ter);
-                    ter->setTransform(Vec3d((i-4)*nSize+nSize*0.5,0,(j-4)*nSize+nSize*0.5));
-                }
-            }
-        }
-        cout << 8*nSize << "m" << endl;
-    };
+    layout.currentMaxLODlvl++;
+    addOuterRing(1);
 
-    //innerQuad
-    fac = 1;
-    for (int i = 0; i < 8; i++){
-        for (int j = 0; j < 8; j++){
-            id = "_inQ" + toString(i) + toString(j);
-            auto ter = generateTerrain( id , size );
-            atlas->addChild(ter);
-            ter->setTransform(Vec3d((i-4)*size+size*0.5,0,(j-4)*size+size*0.5));
-        }
-    }
-    /*fac = fac * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2;
-    addRing();*/
+    layout.currentMaxLODlvl++;
+    addOuterRing(2);
+/*
+    layout.currentMaxLODlvl++;
+    addOuterRing(3);
 
-    for (int r = 0; r < 8; r++){
-        addRing();
-    }
-
-
+*/
     //geo.setTransform(Vec3d(0,0,0));
 
     //Vec3d nor;
--- src/addons/WorldGenerator/GIS/VRAtlas.h
+++ src/addons/WorldGenerator/GIS/VRAtlas.h
@@ -15,12 +15,48 @@ OSG_BEGIN_NAMESPACE;
 
 class VRAtlas : public std::enable_shared_from_this<VRAtlas>  {
     private:
+        enum TYPE {
+            INNERQUAD = 0,
+            INNERRING = 1,
+            OUTERRING = 2
+        };
+        struct Patch {
+            string id;
+            int LODlvl;
+            VRTerrainPtr terrain;
+
+            Patch(string sid, int lvl, VRTerrainPtr ter);
+            Patch();
+            ~Patch();
+        };
+        struct Layout {
+            int currentLODlvl = 0;
+            int currentMaxLODlvl = 0;
+            Vec3d currentOrigin = Vec3d(0,0,0);
+            list<vector<Patch>> rings;
+            vector<Patch> innerQuad;
+            vector<Patch> innerRing;
+            vector<Patch> outerRing;
+            list<Patch> toDestroy;
+            list<Patch> toGenerate;
+        };
+        float size = 64.0;
+        int LODMax = 3;
+        int patchcount = 0;
+
         string filepath;
         VRTransformPtr atlas;
         VRUpdateCbPtr updatePtr;
+        Layout layout;
         void update();
+        void shiftLayout();
+        void downSize();
+        void upSize();
+        void addInnerQuad(int lvl);
+        void addInnerRing(int lvl);
+        void addOuterRing(int lvl);
         VRGeometryPtr generatePatch(string id);
-        VRTerrainPtr generateTerrain(string id, float edgeLength);
+        VRTerrainPtr generateTerrain(string id, int lvl, float edgeLength);
 
     public:
         VRAtlas();
