88b3a6531c0bc066c454cfa99e11cb349b9c0f8e
Victor Haefner <victor.haefner@gmail.com>
2021-02-22 11:42:45 +0100

    moved version to about dialog

--- src/version/version.cpp
+++ src/version/version.cpp
@@ -3,4 +3,4 @@
 // version.h is touched each time
 //  this is why the function is defined here
 
-const char* getVersionString() { return " V: " PVR_COMMIT_ID " " PVR_COMMIT_TIME; }
+const char* getVersionString() { return PVR_COMMIT_ID "\n" PVR_COMMIT_TIME; }
