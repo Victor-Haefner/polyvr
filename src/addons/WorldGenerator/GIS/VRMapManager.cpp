63c8ef7b1f276decdea7b41b80de792718b51198
Victor Haefner <victor.haefner@gmail.com>
2021-03-25 15:17:49 +0100

    map manager now can handle any kind of map files

--- src/addons/WorldGenerator/GIS/VRMapManager.cpp
+++ src/addons/WorldGenerator/GIS/VRMapManager.cpp
@@ -19,49 +19,63 @@ VRMapManagerPtr VRMapManager::create() { return VRMapManagerPtr( new VRMapManage
 VRMapManagerPtr VRMapManager::ptr() { return static_pointer_cast<VRMapManager>(shared_from_this()); }
 
 void VRMapManager::setServer(string s) { server = s; }
-void VRMapManager::setVault(string v) { vault = v; }
 
-string VRMapManager::getMap(double N, double E, double S, VRMapCbPtr mcb) {
-    string filename = constructFilename(N,E,S);
-    if (exists(filename)) {
-        if (mcb) {
-            auto data = VRMapDescriptor::create(N,E,S,filename);
-            (*mcb)(data);
+void VRMapManager::addMapType(int ID, string vault, string servScript, string fileExt) {
+    mapTypes[ID].ID = ID;
+    mapTypes[ID].vault = vault;
+    mapTypes[ID].servScript = servScript;
+    mapTypes[ID].fileExt = fileExt;
+}
+
+VRMapDescriptorPtr VRMapManager::getMap(double N, double E, double S, vector<int> types, VRMapCbPtr mcb) {
+    VRMapDescriptorPtr data = VRMapDescriptor::create(N,E,S);
+
+    for (auto mapType : types) {
+        string filename = constructFilename(N,E,S, mapType);
+        if (exists(filename)) data->setMap(mapType, filename);
+        else {
+            requestFile(filename, N,E,S, mapType, types, mcb);
+            if (!mcb) data->setMap(mapType, filename);
+            else {
+                data->setCompleteness(false);
+                break;
+            }
         }
-        return filename;
     }
-    else requestFile("Topology_GetMap.php", filename, N,E,S, 0, mcb);
-    return filename;
+
+    if (mcb && data->isComplete()) {
+        auto fkt = VRUpdateCb::create("map manager job", bind(&VRMapManager::triggerCB, this, mcb, data));
+        VRScene::getCurrent()->queueJob(fkt);
+    }
+    return data;
 }
 
 // --= utilities =--
 
-void VRMapManager::handleRequestAnswer(VRRestResponsePtr response, string filename, VRMapCbPtr mcb, double N, double E, double S, int mapType) {
+void VRMapManager::handleRequestAnswer(VRRestResponsePtr response, string filename, VRMapCbPtr mcb, double N, double E, double S, vector<int> types) {
     //cout << " response: " << response->getStatus() << endl;
     cout << " map data response, data size: " << response->getData().size() << endl;
     cout << " store map data in: " << filename << endl;
 
     // store result in file 'filename'
     storeFile(filename, response->getData());
-    auto data = VRMapDescriptor::create();
-    data->setParameters(N, E, S);
-    data->setMap(mapType, filename);
-    auto fkt = VRUpdateCb::create("map manager job", bind(&VRMapManager::triggerCB, this, mcb, data));
-    VRScene::getCurrent()->queueJob(fkt);
+    if (mcb) getMap(N, E, S, types, mcb);
 }
 
-void VRMapManager::requestFile(string script, string filename, double N, double E, double S, int mapType, VRMapCbPtr mcb) {
+void VRMapManager::requestFile(string filename, double N, double E, double S, int mapType, vector<int> types, VRMapCbPtr mcb) {
     string sN = toString(N,3);
     string sE = toString(E,3);
     string sS = toString(S,3);
+
+    string script = mapTypes[mapType].servScript;
     string req = server+script+"?N="+sN+"&E="+sE+"&S="+sS;  // N=%.3f&E=%.3f&S=%.3f" % (N,E,S)
     cout << " VRMapManager request: " << req << endl;
 
     // launch get request
     if (!client) client = VRRestClient::create();
 
-    if (mcb) client->getAsync(req, VRRestCb::create("asyncGet", bind(&VRMapManager::handleRequestAnswer, this, placeholders::_1, filename, mcb, N, E, S, mapType)));
-    else     client->get(req);
+    if (mcb) client->getAsync(req, VRRestCb::create("asyncGet", bind(&VRMapManager::handleRequestAnswer, this, placeholders::_1, filename, mcb, N, E, S, types)));
+    else handleRequestAnswer(client->get(req), filename, mcb, N, E, S, types);
 }
 
 void VRMapManager::triggerCB(VRMapCbPtr mcb, VRMapDescriptorPtr data) {
@@ -69,20 +83,23 @@ void VRMapManager::triggerCB(VRMapCbPtr mcb, VRMapDescriptorPtr data) {
 }
 
 void VRMapManager::storeFile(const string& filename, const string& data) {
+    if (data == "") return;
     ofstream f;
     f.open(filename);
     f << data;
     f.close();
 }
 
-string VRMapManager::constructFilename(double N, double E, double S) {
+string VRMapManager::constructFilename(double N, double E, double S, int mapType) {
     //cout << "VRMapManager::constructFilename " << Vec3d(N, E, S) << endl;
     string sN = toString(N,3);
     string sE = toString(E,3);
     string sS = toString(S,3);
 
+    string vault = mapTypes[mapType].vault;
+    string fileExt = mapTypes[mapType].fileExt;
     string folder = vault+"/"+sS;
-    string filename = folder+"/N"+sN+"E"+sE+"S"+sS+".hgt";
+    string filename = folder+"/N"+sN+"E"+sE+"S"+sS+fileExt;
     if (!exists(folder)) makedir(folder);
     return filename;
 }
@@ -96,9 +113,8 @@ VRMapDescriptor::~VRMapDescriptor() {}
 
 VRMapDescriptorPtr VRMapDescriptor::create() { return VRMapDescriptorPtr( new VRMapDescriptor() ); }
 
-VRMapDescriptorPtr VRMapDescriptor::create(double n, double e, double s, string f) {
+VRMapDescriptorPtr VRMapDescriptor::create(double n, double e, double s) {
     auto d = create();
-    d->setMap(0, f);
     d->setParameters(n,e,s);
     return d;
 }
@@ -113,3 +129,5 @@ Vec3d VRMapDescriptor::getParameters() { return Vec3d(N, E, S); }
 void VRMapDescriptor::setMap(int i, string s) { layers[i] = s; }
 void VRMapDescriptor::setParameters(double n, double e, double s) { N = n; E = e; S = s; }
 
+void VRMapDescriptor::setCompleteness(bool c) { complete = c; }
+bool VRMapDescriptor::isComplete() { return complete; }
