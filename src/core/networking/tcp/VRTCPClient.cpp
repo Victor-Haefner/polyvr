b2fbc7f87837340f305e299beca41af9952f8eb7
Victor Haefner <victor.haefner@gmail.com>
2021-04-21 10:56:48 +0200

    working on TCP client shutdown

--- src/core/networking/tcp/VRTCPClient.cpp
+++ src/core/networking/tcp/VRTCPClient.cpp
@@ -22,7 +22,8 @@ using ip::tcp;
 
 class TCPClient {
     private:
-        typedef shared_ptr<boost::asio::ip::tcp::socket> SocketPtr;
+        typedef shared_ptr<tcp::socket> SocketPtr;
+        typedef shared_ptr<tcp::acceptor> AcceptorPtr;
 
         boost::asio::io_service io_service;
         boost::asio::io_service::work worker;
@@ -38,6 +39,7 @@ class TCPClient {
         thread tunnelRead;
         SocketPtr aSocket;
         SocketPtr cSocket;
+        AcceptorPtr acceptor;
         bool stop = false;
 
         function<void (string)> onMessageCb;
@@ -53,9 +55,9 @@ class TCPClient {
             return res;
         }
 
-        void read_handler(const boost::system::error_code& ec, size_t N) {
+        bool read_handler(const boost::system::error_code& ec, size_t N) {
             //cout << "TCPClient, read_handler, got: " << N << endl;
-            if (ec) cout << "TCPClient, read_handler failed with: " << ec.message() << endl;
+            if (ec) { cout << "TCPClient, read_handler failed with: " << ec.message() << endl; return false; }
 
             size_t gN = guard.size();
             if (!ec && N > gN) {
@@ -68,18 +70,21 @@ class TCPClient {
                 //cout << "TCPClient, received: " << data << ", cb: " << endl;
                 if (onMessageCb) onMessageCb(data);
             } else {}
+
+            return true;
         }
 
-        void read(bool block = false) {
+        bool read(bool block = false) {
             if (!block) {
                 if (guard == "") boost::asio::async_read( *socket, buffer, boost::asio::transfer_at_least(1), bind(&TCPClient::read_handler, this, std::placeholders::_1, std::placeholders::_2) );
                 else boost::asio::async_read_until( *socket, buffer, guard, bind(&TCPClient::read_handler, this, std::placeholders::_1, std::placeholders::_2) );
+                return true;
             } else {
                 boost::system::error_code ec;
                 size_t N = 0;
                 if (guard == "") N = boost::asio::read( *socket, buffer, boost::asio::transfer_at_least(1), ec);
                 else N = boost::asio::read_until( *socket, buffer, guard, ec);
-                read_handler(ec, N);
+                return read_handler(ec, N);
             }
         }
 
@@ -169,24 +174,26 @@ class TCPClient {
         void connectToPeer(int lPort, string rIP, int rPort) {
             string lIP = VRTCPUtils::getLocalIP();
             cout << "TCPClient::connectToPeer " << lIP << ":" << lPort << ", to " << rIP << ":" << rPort << endl;
-            tunnelAccept = thread([this, lPort]() { acceptHolePunching(lPort); });
+            //tunnelAccept = thread([this, lPort]() { acceptHolePunching(lPort); }); // needed??? if yes, then TODO: fix close (timeout)!
             tunnelConnect = thread([this, lIP, lPort, rIP, rPort]() { connectHolePunching(lIP, lPort, rIP, rPort); });
 		}
 
 		void finalizeP2P() {
             //cout << " p2p socket states: " << aSocket->is_open() << " " << cSocket->is_open() << endl;
 
-            if (aSocket->is_open()) { socket = aSocket; /*cSocket->cancel();*/ }
-            if (cSocket->is_open()) { socket = cSocket; /*aSocket->cancel();*/ }
+            //if (aSocket->is_open()) socket = aSocket;
+            if (cSocket->is_open()) socket = cSocket;
+            if (acceptor) acceptor->cancel();
             aSocket.reset();
             cSocket.reset();
+            acceptor.reset();
 
             cout << "finalizeP2P, close threads " << this << endl;
             //tunnelAccept.join();
             //tunnelConnect.join();
             cout << " finalizeP2P, close threads done" << endl;
 
-            tunnelRead = thread([this]() { while(true) read(true); });
+            tunnelRead = thread([this]() { bool run = true; while(run) run = read(true); });
 
             if (onConnectCb) onConnectCb();
 		}
@@ -194,32 +201,33 @@ class TCPClient {
         void acceptHolePunching(int port) {
             //cout << "TCPClient::acceptHolePunching on " << port << endl;
             boost::asio::ip::tcp::endpoint ep(boost::asio::ip::tcp::v4(), port);
-            boost::asio::ip::tcp::acceptor acceptor(io_service, ep.protocol());
+            acceptor = AcceptorPtr( new tcp::acceptor(io_service, ep.protocol()) );
 
             //cout << " accept acceptor bind on endpoint " << ep << endl;
             boost::asio::socket_base::reuse_address reuseAddress(true);
             boost::asio::detail::socket_option::boolean<SOL_SOCKET, SO_REUSEPORT> reusePort(true);
-            acceptor.set_option(reuseAddress); //    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            acceptor.set_option(reusePort); //    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            boost::asio::socket_base::enable_connection_aborted enable_abort(true);
+            acceptor->set_option(reuseAddress); //    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            acceptor->set_option(reusePort); //    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            acceptor->set_option(enable_abort);
 
             boost::system::error_code ec;
-            acceptor.bind(ep, ec); //    s.bind(('', port))
+            acceptor->bind(ep, ec); //    s.bind(('', port))
 
             //Handling Errors
             if (ec.value() != 0) {
                 std::cout << "Failed to bind the acceptor socket." << "Error code = " << ec.value() << ". Message: " << ec.message() << endl;
             }
 
-            acceptor.listen(1); //    s.listen(1)
+            acceptor->listen(1); //    s.listen(1)
             aSocket = SocketPtr( new tcp::socket(io_service) );//    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             //    s.settimeout(5)
-
 
             while (!stop) {  //    while not STOP.is_set():
                 try {  //        try:
                     cout << " try accept on " << port << endl;
-                    acceptor.accept(*aSocket); //            conn, addr = s.accept()
-                    //cout << "  --- accepted connection! ---" << endl;
+                    acceptor->accept(*aSocket); //            conn, addr = s.accept()
+                    cout << "  --- accepted connection! ---" << endl;
                 }
                 catch (boost::system::system_error& e) { //        except socket.timeout:
                     cout << "Exception at VRSyncConnection::connect2. Exception Nr. " << e.what() << endl;
