f1b6c4fda28badc131d1b03a1293987c88e60604
Victor Haefner <victor.haefner@gmail.com>
2021-05-28 19:45:31 +0300

    added latency in pipe sim

--- src/addons/Engineering/VRPipeSystem.cpp
+++ src/addons/Engineering/VRPipeSystem.cpp
@@ -195,6 +195,8 @@ void VRPipeSystem::update() {
     double dT = 1.0/60; // TODO: use animation
     double dt = dT/subSteps;
 
+    double latency = 0.001;
+
     for (int i=0; i<subSteps; i++) {
 
         for (auto n : nodes) { // traverse nodes, change pressure in segments
@@ -329,8 +331,8 @@ void VRPipeSystem::update() {
                 }
 
                 if (closed) {
-                    pipe1->dFl = -pipe1->flow;
-                    pipe2->dFl = -pipe2->flow;
+                    pipe1->dFl = -pipe1->flow*latency;
+                    pipe2->dFl = -pipe2->flow*latency;
                     pipe1->flowBlocked = true;
                     pipe2->flowBlocked = true;
                 }
@@ -369,17 +371,17 @@ void VRPipeSystem::update() {
                 for (auto eID : inFlowPipeIDs  ) maxInFlow  += abs(segments[eID]->flow + segments[eID]->dFl);
                 for (auto eID : outFlowPipeIDs ) maxOutFlow += abs(segments[eID]->flow + segments[eID]->dFl);
                 double maxFlow = min(maxInFlow, maxOutFlow);
-                if (maxFlow > 1e-6) {
+                if (maxFlow > 1e-6 && maxInFlow > 1e-6 && maxOutFlow > 1e-6) {
                     double inPart  = maxFlow/maxInFlow;
                     double outPart = maxFlow/maxOutFlow;
                     //for (auto eID :  inFlowPipeIDs ) segments[eID]->dFl *= inPart;
                     //for (auto eID : outFlowPipeIDs ) segments[eID]->dFl *= outPart;
-                    for (auto eID :  inFlowPipeIDs ) segments[eID]->dFl = (segments[eID]->flow + segments[eID]->dFl)*inPart  - segments[eID]->flow;
-                    for (auto eID : outFlowPipeIDs ) segments[eID]->dFl = (segments[eID]->flow + segments[eID]->dFl)*outPart - segments[eID]->flow;
+                    for (auto eID :  inFlowPipeIDs ) segments[eID]->dFl = (segments[eID]->flow*latency + segments[eID]->dFl)*inPart  - segments[eID]->flow*latency;
+                    for (auto eID : outFlowPipeIDs ) segments[eID]->dFl = (segments[eID]->flow*latency + segments[eID]->dFl)*outPart - segments[eID]->flow*latency;
                     if (abs(inPart-1.0) > 1e-3 || abs(outPart-1.0) > 1e-3) flowCheck = true;
                 } else { // no flow
-                    for (auto eID :  inFlowPipeIDs ) segments[eID]->dFl = -segments[eID]->flow;
-                    for (auto eID : outFlowPipeIDs ) segments[eID]->dFl = -segments[eID]->flow;
+                    for (auto eID :  inFlowPipeIDs ) segments[eID]->dFl = -segments[eID]->flow*latency;
+                    for (auto eID : outFlowPipeIDs ) segments[eID]->dFl = -segments[eID]->flow*latency;
                     flowCheck = true;
                 }
 
@@ -400,7 +402,11 @@ void VRPipeSystem::update() {
             string n1 = nodes[e.from]->entity->getName();
             string n2 = nodes[e.to]->entity->getName();
 
-            if (isNan(s.second->dFl)) s.second->dFl = 0;
+            if (isNan(s.second->dFl)) {
+                s.second->dFl = 0;
+                cout << "Warning in Pipe simulation! dFL is NaN!" << endl;
+            }
+
             s.second->flow += s.second->dFl;  // pipe flow change in m³ / s
             //if (abs(s.second->dFl) > 1e-9 || 1)
             //    cout << " flow +" << s.second->dFl << " -> " << s.second->flow << " (" << n1 << "->" << n2 << ") blocked? " << s.second->flowBlocked << endl;
