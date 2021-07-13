574580d97017c9a657771798359d404643243590
Victor Haefner <victor.haefner@gmail.com>
2021-07-13 15:18:40 +0200

    stuff

--- src/addons/WorldGenerator/terrain/VRTerrain.cpp
+++ src/addons/WorldGenerator/terrain/VRTerrain.cpp
@@ -806,73 +806,6 @@ uniform int channel;
 uniform float heightoffset;
 uniform mat4 OSGModelViewProjectionMatrix;
 
-#ifdef __EMSCRIPTEN__
-// Denormalize 8-bit color channels to integers in the range 0 to 255.
-ivec4 floatsToBytes(vec4 inputFloats) {
-  ivec4 bytes = ivec4(inputFloats * 255.0);
-  return bytes.abgr;
-}
-
-// Break the four bytes down into an array of 32 bits.
-void bytesToBits(const in ivec4 bytes, out bool bits[32]) {
-  for (int channelIndex = 0; channelIndex < 4; ++channelIndex) {
-    float acc = float(bytes[channelIndex]);
-    for (int indexInByte = 7; indexInByte >= 0; --indexInByte) {
-      float powerOfTwo = exp2(float(indexInByte));
-      bool bit = acc >= powerOfTwo;
-      bits[channelIndex * 8 + (7 - indexInByte)] = bit;
-      acc = mod(acc, powerOfTwo);
-    }
-  }
-}
-
-// Compute the exponent of the 32-bit float.
-float getExponent(bool bits[32]) {
-  const int startIndex = 1;
-  const int bitStringLength = 8;
-  const int endBeforeIndex = startIndex + bitStringLength;
-  float acc = 0.0;
-  int pow2 = bitStringLength - 1;
-  for (int bitIndex = startIndex; bitIndex < endBeforeIndex; ++bitIndex) {
-    acc += float(bits[bitIndex]) * exp2(float(pow2--));
-  }
-  return acc;
-}
-
-// Compute the mantissa of the 32-bit float.
-float getMantissa(bool bits[32], bool subnormal) {
-  const int startIndex = 9;
-  const int bitStringLength = 23;
-  const int endBeforeIndex = startIndex + bitStringLength;
-  // Leading/implicit/hidden bit convention:
-  // If the number is not subnormal (with exponent 0), we add a leading 1 digit.
-  float acc = float(!subnormal) * exp2(float(bitStringLength));
-  int pow2 = bitStringLength - 1;
-  for (int bitIndex = startIndex; bitIndex < endBeforeIndex; ++bitIndex) {
-    acc += float(bits[bitIndex]) * exp2(float(pow2--));
-  }
-  return acc;
-}
-
-// Parse the float from its 32 bits.
-float bitsToFloat(bool bits[32]) {
-  float signBit = float(bits[0]) * -2.0 + 1.0;
-  float exponent = getExponent(bits);
-  bool subnormal = abs(exponent - 0.0) < 0.01;
-  float mantissa = getMantissa(bits, subnormal);
-  float exponentBias = 127.0;
-  return signBit * mantissa * exp2(exponent - exponentBias - 23.0);
-}
-
-// Decode a 32-bit float from the RGBA color channels of a texel.
-float rgbaToFloat(vec4 texelRGBA) {
-  ivec4 rgbaBytes = floatsToBytes(texelRGBA);
-  bool bits[32];
-  bytesToBits(rgbaBytes, bits);
-  return bitsToFloat(bits);
-}
-#endif
-
 void main(void) {
     vVertex = osg_Vertex;
     vNormal = osg_Normal;
@@ -881,14 +814,10 @@ void main(void) {
     vec4 texData = texture2D(texture, osg_MultiTexCoord0);
     vColor = texData;
     float height = texData.a;
-#ifdef __EMSCRIPTEN__
-    height = rgbaToFloat(texData);
-#else
     if (channel == 0) height = texData.r;
     if (channel == 1) height = texData.g;
     if (channel == 2) height = texData.b;
     if (channel == 3) height = texData.a;
-#endif
     vec4 tePosition = osg_Vertex;
     //float nheight = osg_MultiTexCoord0.x * 10000.0;
     float nheight = (height - heightoffset);//*100.0;
