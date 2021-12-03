58b37a98d982a63c8aad8d2a56894f59ec926853
Victor Haefner <victor.haefner@gmail.com>
2021-12-03 09:17:16 +0100

    added workaround for multiple polyvr processes

--- src/core/scripting/VRPyMobile.cpp
+++ src/core/scripting/VRPyMobile.cpp
@@ -11,6 +11,7 @@ PyMethodDef VRPyServer::methods[] = {
     {"answer", PyWrap(Server, answerWebSocket, "Answer web socket - answer(int id, str message)\n use the device key as id, id = dev.getKey()", void, int, string) },
     {"openWebSocket", PyWrapOpt(Server, openWebSocket, "Opens a websocket, returns connect ID.", "", int, string, string) },
     {"getClients", PyWrap(Server, getClients, "Get list of current client IDs", myMap ) },
+    {"getPort", PyWrap(Server, getPort, "Get port this server is listening on", int ) },
     {NULL}  /* Sentinel */
 };
 
