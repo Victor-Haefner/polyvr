1b91115cc901590178520cdfc06d7fdbaf6cc347
MReuter <ujpvs@student.kit.edu>
2023-07-07 14:09:17 +0200

    fix

--- src/addons/Engineering/Machining/VRPyMachining.cpp
+++ src/addons/Engineering/Machining/VRPyMachining.cpp
@@ -24,6 +24,7 @@ PyMethodDef VRPyMachiningCode::methods[] = {
 };
 
 PyMethodDef VRPyMachiningKinematics::methods[] = {
+    {"setEndEffector", PyWrap(MachiningKinematics, setEndEffector, "Set end effector position", void, PosePtr) },
     {NULL}  /* Sentinel */
 };
 
