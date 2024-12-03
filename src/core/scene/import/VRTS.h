4d328b4211b4de0f480a23702f9574381fd00b6d
Victor Haefner <victor.haefner@gmail.com>
2024-12-03 17:49:34 +0100

    added offset to ts import

--- src/core/scene/import/VRTS.h
+++ src/core/scene/import/VRTS.h
@@ -3,12 +3,13 @@
 
 #include <OpenSG/OSGConfig.h>
 #include <string>
+#include <map>
 #include "core/objects/VRObjectFwd.h"
 
 OSG_BEGIN_NAMESPACE;
 using namespace std;
 
-void loadTS(string path, VRTransformPtr res);
+void loadTS(string path, VRTransformPtr res, map<string, string> options);
 void writeTS(VRGeometryPtr geo, string path);
 
 OSG_END_NAMESPACE;
