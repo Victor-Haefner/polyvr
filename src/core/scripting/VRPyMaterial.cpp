ea58b75c61f035f04690bd7adfac9682e0e2e7a6
Victor Haefner <victor.haefner@gmail.com>
2025-12-19 13:09:58 +0100

    refactored material py bindings

--- src/core/scripting/VRPyMaterial.cpp
+++ src/core/scripting/VRPyMaterial.cpp
@@ -40,32 +40,32 @@ PyMethodDef VRPyMaterial::methods[] = {
     {"setEmission", PyWrapPack(Material, setEmission, "Sets the emission color", void, Color3f ) },
     {"getSpecular", PyWrap(Material, getSpecular, "Returns the specular color", Color3f ) },
     {"setSpecular", PyWrapPack(Material, setSpecular, "Sets the specular color", void, Color3f ) },
-    {"getTransparency", (PyCFunction)VRPyMaterial::getTransparency, METH_NOARGS, "Returns the transparency - f getTransparency()" },
-    {"setTransparency", (PyCFunction)VRPyMaterial::setTransparency, METH_VARARGS, "Sets the transparency - setTransparency(f)" },
+    {"getTransparency", PyWrap(Material, getTransparency, "Returns the transparency - f getTransparency()", float ) },
+    {"setTransparency", PyWrap(Material, setTransparency, "Sets the transparency - setTransparency(f)", void, float ) },
     {"setClipPlane", PyWrap(Material, setClipPlane, "Set the clip chunk, (active, plane eq., beacon)", void, bool, Vec4d, VRTransformPtr) },
     {"addClipPlane", PyWrap(Material, addClipPlane, "Add a clip chunk, (plane eq., beacon)", void, Vec4d, VRTransformPtr) },
     {"setDepthTest", PyWrap(Material, setDepthTest, "Sets the depth test function\t\n'GL_ALWAYS'", void, int ) },
     {"getDepthTest", PyWrap(Material, getDepthTest, "Get depth test function", int ) },
-    {"clearTransparency", (PyCFunction)VRPyMaterial::clearTransparency, METH_NOARGS, "Clears the transparency channel - clearTransparency()" },
-    {"enableTransparency", (PyCFunction)VRPyMaterial::enableTransparency, METH_NOARGS, "Enables the transparency channel - enableTransparency()" },
-    {"getShininess", (PyCFunction)VRPyMaterial::getShininess, METH_NOARGS, "Returns the shininess - f getShininess()" },
-    {"setShininess", (PyCFunction)VRPyMaterial::setShininess, METH_VARARGS, "Sets the shininess - setShininess(f)" },
-    {"setPointSize", (PyCFunction)VRPyMaterial::setPointSize, METH_VARARGS, "Sets the GL point size - setPointSize(i)" },
-    {"setLineWidth", (PyCFunction)VRPyMaterial::setLineWidth, METH_VARARGS, "Sets the GL line width - setLineWidth(i)" },
-    {"getTexture", (PyCFunction)VRPyMaterial::getTexture, METH_VARARGS, "Get the texture - texture getTexture( int unit = 0 )" },
+    {"clearTransparency", PyWrapOpt(Material, clearTransparency, "Clears the transparency channel - clearTransparency()", "1", void, bool ) },
+    {"enableTransparency", PyWrapOpt(Material, enableTransparency, "Enables the transparency channel - enableTransparency()", "1", void, bool ) },
+    {"getShininess", PyWrap(Material, getShininess, "Returns the shininess - f getShininess()", float ) },
+    {"setShininess", PyWrap(Material, setShininess, "Sets the shininess - setShininess(f)", void, float ) },
+    {"setPointSize", PyWrapOpt(Material, setPointSize, "Sets the GL point size - setPointSize(i, smooth = true)", "1", void, int, bool ) },
+    {"setLineWidth", PyWrapOpt(Material, setLineWidth, "Sets the GL line width - setLineWidth(i, smooth = true)", "1", void, int, bool ) },
+    {"getTexture", PyWrapOpt(Material, getTexture, "Get the texture - texture getTexture( int unit = 0 )", "0", VRTexturePtr, int ) },
     {"setQRCode", PyWrap(Material, setQRCode, "Encode a string as QR code texture - setQRCode(string, fg[r,g,b], bg[r,g,b], offset)", void, string, Color3f, Color3f, int) },
-    {"setMagMinFilter", (PyCFunction)VRPyMaterial::setMagMinFilter, METH_VARARGS, "Set the mag and min filtering mode - setMagMinFilter( mag, min | int unit )\n possible values for mag are GL_X && min can be GL_X || GL_X_MIPMAP_Y, where X && Y can be NEAREST || LINEAR" },
-    {"setTextureWrapping", (PyCFunction)VRPyMaterial::setTextureWrapping, METH_VARARGS, "Set the texture wrap in u and v - setTextureWrapping( wrapU, wrapV | int unit )\n possible values for wrap are 'GL_CLAMP_TO_EDGE', 'GL_CLAMP_TO_BORDER', 'GL_CLAMP', 'GL_REPEAT'" },
-    {"setVertexProgram", (PyCFunction)VRPyMaterial::setVertexProgram, METH_VARARGS, "Set vertex program - setVertexProgram( myScript )" },
-    {"setFragmentProgram", (PyCFunction)VRPyMaterial::setFragmentProgram, METH_VARARGS, "Set fragment program - setFragmentProgram( myScript | bool deferred )" },
-    {"setGeometryProgram", (PyCFunction)VRPyMaterial::setGeometryProgram, METH_VARARGS, "Set geometry program - setGeometryProgram( myScript )" },
-    {"setTessControlProgram", (PyCFunction)VRPyMaterial::setTessControlProgram, METH_VARARGS, "Set tess control program - setTessControlProgram( myScript )" },
-    {"setTessEvaluationProgram", (PyCFunction)VRPyMaterial::setTessEvaluationProgram, METH_VARARGS, "Set tess evaluation program - setTessEvaluationProgram( myScript )" },
+    {"setMagMinFilter", PyWrapOpt(Material, setMagMinFilter, "Set the mag and min filtering mode - setMagMinFilter( mag, min | int unit )\n possible values for mag are GL_X && min can be GL_X || GL_X_MIPMAP_Y, where X && Y can be NEAREST || LINEAR", "0", void, int, int, int ) },
+    {"setTextureWrapping", PyWrap(Material, setTextureWrapping, "Set the texture wrap in u and v - setTextureWrapping( wrapU, wrapV | int unit )\n possible values for wrap are 'GL_CLAMP_TO_EDGE', 'GL_CLAMP_TO_BORDER', 'GL_CLAMP', 'GL_REPEAT'", void, int, int, int ) },
+    {"setVertexProgram", PyWrap(Material, setVertexScript, "Set vertex program - setVertexProgram( myScript )", void, string ) },
+    {"setFragmentProgram", PyWrap(Material, setFragmentScript, "Set fragment program - setFragmentProgram( myScript | bool deferred )", void, string, bool ) },
+    {"setGeometryProgram", PyWrap(Material, setGeometryScript, "Set geometry program - setGeometryProgram( myScript )", void, string ) },
+    {"setTessControlProgram", PyWrap(Material, setTessControlScript, "Set tess control program - setTessControlProgram( myScript )", void, string ) },
+    {"setTessEvaluationProgram", PyWrap(Material, setTessEvaluationScript, "Set tess evaluation program - setTessEvaluationProgram( myScript )", void, string ) },
     {"ignoreMeshColors", PyWrapOpt( Material, ignoreMeshColors, "Allows to ignore vertex colors, only works if lit", "1", void, bool ) },
     {"doesIgnoreMeshColors", PyWrap( Material, doesIgnoreMeshColors, "Check if ignoring vertex colors", bool ) },
     {"setWireFrame", PyWrap( Material, setWireFrame, "Set front and back to wireframe", void, bool ) },
     {"isWireFrame", PyWrap( Material, isWireFrame, "Check if wireframe", bool ) },
-    {"setLit", (PyCFunction)VRPyMaterial::setLit, METH_VARARGS, "Set if geometry is lit - setLit(bool)" },
+    {"setLit", PyWrap(Material, setLit, "Set if geometry is lit - setLit(bool)", void, bool ) },
     {"isLit", PyWrap( Material, isLit, "Get if geometry is lit", bool ) },
     {"addPass", PyWrap( Material, addPass, "Add a new pass", int ) },
     {"setActivePass", PyWrap( Material, setActivePass, "Set active pass", void, int ) },
@@ -73,12 +73,12 @@ PyMethodDef VRPyMaterial::methods[] = {
     {"getNPasses", PyWrap( Material, getNPasses, "Get number of passes", int ) },
     {"setFrontBackModes", PyWrap(Material, setFrontBackModes, "Set the draw mode of front and back faces - setFrontBackModes(front, back)\n\tmode can be: GL_NONE, GL_FILL, GL_LINE, GL_BACK", void, int, int) },
     {"setZOffset", PyWrap(Material, setZOffset, "Set Z buffer offset and bias, set to 1/1 to push behind or -1/-1 to pull to front", void, float, float) },
-    {"setSortKey", (PyCFunction)VRPyMaterial::setSortKey, METH_VARARGS, "Set the sort key, higher key means rendered later, for example 0 is rendered first, 1 second.." },
+    {"setSortKey", PyWrap(Material, setSortKey, "Set the sort key, higher key means rendered later, for example 0 is rendered first, 1 second..", void, int ) },
     {"setTexture", (PyCFunction)VRPyMaterial::setTexture, METH_VARARGS, "Set the texture - setTexture(str path, int pos = 0)"
                                                                         "\n\t setTexture( Texture, int pos = 0)"
                                                                         "\n\t setTexture([[r,g,b]], [xN, yN, zN], bool isFloat)"
                                                                         "\n\t setTexture([[r,g,b,a]], [xN, yN, zN], bool isFloat)" },
-    {"setTextureType", (PyCFunction)VRPyMaterial::setTextureType, METH_VARARGS, "Set the texture type - setTexture(str type)\n types are: 'Normal, 'SphereEnv'" },
+    {"setTextureType", PyWrapOpt(Material, setTextureType, "Set the texture type - setTextureType(str type, unit = 0)\n types are: 'Normal, 'SphereEnv'", "0", void, string, int ) },
     {"setStencilBuffer", PyWrap(Material, setStencilBuffer
         , "Use stencil buffer, (doClear, value, mask, func, opFail, opZFail, opPass)"
         "\n\t doClear clears the buffer to 0"
@@ -95,7 +95,7 @@ PyMethodDef VRPyMaterial::methods[] = {
         "\n\t OSGNodeWorldBoxMax OSGNodeWorldBoxCenter OSGActiveLightsMask OSGLight0Active OSGLight1Active OSGLight2Active"
         "\n\t OSGLight3Active OSGLight4Active OSGLight5Active OSGLight6Active OSGLight7Active }",
         void, string) },
-    {"setDefaultVertexShader", (PyCFunction)VRPyMaterial::setDefaultVertexShader, METH_NOARGS, "Set a default vertex shader - setDefaultVertexShader()" },
+    {"setDefaultVertexShader", PyWrap(Material, setDefaultVertexShader, "Set a default vertex shader - setDefaultVertexShader()", void ) },
     {"testFix", PyWrap( Material, testFix, "Debug helper", void ) },
     {"setMappingBeacon", PyWrapOpt( Material, setMappingBeacon, "Set beacon for cube mapping", "0", void, VRObjectPtr, int ) },
     {"setMappingPlanes", PyWrapOpt( Material, setMappingPlanes, "Set planes for cube mapping", "0", void, Vec4d, Vec4d, Vec4d, Vec4d, int ) },
@@ -123,39 +123,6 @@ PyMethodDef VRPyMaterial::methods[] = {
     {NULL}  /* Sentinel */
 };
 
-PyObject* VRPyMaterial::enableTransparency(VRPyMaterial* self) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::enableTransparency, C obj is invalid"); return NULL; }
-	self->objPtr->enableTransparency(true);
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::clearTransparency(VRPyMaterial* self) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::clearTransparency, C obj is invalid"); return NULL; }
-	self->objPtr->clearTransparency(true);
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setDefaultVertexShader(VRPyMaterial* self) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setDefaultVertexShader, C obj is invalid"); return NULL; }
-	self->objPtr->setDefaultVertexShader();
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setDepthTest(VRPyMaterial* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-	const char* s = 0;
-    if (! PyArg_ParseTuple(args, "s", (char*)&s)) return NULL;
-	if (s) self->objPtr->setDepthTest( toGLConst(s) );
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::getTexture(VRPyMaterial* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-	int i=0;
-    if (! PyArg_ParseTuple(args, "|i", &i)) return NULL;
-	return VRPyTexture::fromSharedPtr( self->objPtr->getTexture(i) );
-}
-
 PyObject* VRPyMaterial::setShaderParameter(VRPyMaterial* self, PyObject* args) {
     auto mPtr = self->objPtr;
 	if (mPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setShaderParameter, C obj is invalid"); return NULL; }
@@ -169,21 +136,6 @@ PyObject* VRPyMaterial::setShaderParameter(VRPyMaterial* self, PyObject* args) {
     if (PyVec_Check(var, 2, 'f')) { Vec2f v; toValue(var,v); mPtr->setShaderParameter( n, v ); }
     if (PyVec_Check(var, 3, 'f')) { Vec3f v; toValue(var,v); mPtr->setShaderParameter( n, v ); }
     if (PyVec_Check(var, 4, 'f')) { Vec4f v; toValue(var,v); mPtr->setShaderParameter( n, v ); }
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setSortKey(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setSortKey, C obj is invalid"); return NULL; }
-	self->objPtr->setSortKey( parseInt(args) );
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setTextureType(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setTextureType, C obj is invalid"); return NULL; }
-    const char* t = 0;
-    int u = 0;
-    if (! PyArg_ParseTuple(args, "s|i", &t, &u)) return NULL;
-	self->objPtr->setTextureType(t?string(t):"", u);
 	Py_RETURN_TRUE;
 }
 
@@ -231,124 +183,8 @@ PyObject* VRPyMaterial::setTexture(VRPyMaterial* self, PyObject* args) {
 	Py_RETURN_TRUE;
 }
 
-PyObject* VRPyMaterial::setZOffset(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setZOffset, C obj is invalid"); return NULL; }
-	float f,b;
-    if (! PyArg_ParseTuple(args, "ff", &f, &b)) return NULL;
-	self->objPtr->setZOffset(f,b);
-	Py_RETURN_TRUE;
-}
 
-PyObject* VRPyMaterial::setLit(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setLit, C obj is invalid"); return NULL; }
-	self->objPtr->setLit(parseBool(args));
-	Py_RETURN_TRUE;
-}
 
-PyObject* VRPyMaterial::addPass(VRPyMaterial* self) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::addPass, C obj is invalid"); return NULL; }
-	return PyLong_FromLong( self->objPtr->addPass() );
-}
 
-PyObject* VRPyMaterial::remPass(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::remPass, C obj is invalid"); return NULL; }
-	self->objPtr->remPass(parseInt(args));
-	Py_RETURN_TRUE;
-}
 
-PyObject* VRPyMaterial::setActivePass(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setActivePass, C obj is invalid"); return NULL; }
-	self->objPtr->setActivePass(parseInt(args));
-	Py_RETURN_TRUE;
-}
 
-PyObject* VRPyMaterial::setWireFrame(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setWireFrame, C obj is invalid"); return NULL; }
-	self->objPtr->setWireFrame(parseBool(args));
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setMagMinFilter(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setMagMinFilter, C obj is invalid"); return NULL; }
-	PyObject *mag, *min;
-	int unit = 0;
-    if (! PyArg_ParseTuple(args, "OO|i", &mag, &min, &unit)) return NULL;
-	self->objPtr->setMagMinFilter(toGLConst(mag), toGLConst(min), unit);
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setTextureWrapping(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setTextureWrapping, C obj is invalid"); return NULL; }
-	PyObject *ws, *wt;
-	int unit = 0;
-    if (! PyArg_ParseTuple(args, "OO|i", &ws, &wt, &unit)) return NULL;
-	self->objPtr->setTextureWrapping(toGLConst(ws), toGLConst(wt), unit);
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setVertexProgram(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setVertexProgram, C obj is invalid"); return NULL; }
-	self->objPtr->setVertexScript(parseString(args));
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setFragmentProgram(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setFragmentProgram, C obj is invalid"); return NULL; }
-	const char* code = 0;
-	int def = 0;
-    if (! PyArg_ParseTuple(args, "s|i", (char*)&code, &def)) return NULL;
-	self->objPtr->setFragmentScript(code, def);
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setGeometryProgram(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setGeometryProgram, C obj is invalid"); return NULL; }
-	self->objPtr->setGeometryScript(parseString(args));
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setTessControlProgram(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setTessControlProgram, C obj is invalid"); return NULL; }
-	self->objPtr->setTessControlScript(parseString(args));
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setTessEvaluationProgram(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setTessEvaluationProgram, C obj is invalid"); return NULL; }
-	self->objPtr->setTessEvaluationScript(parseString(args));
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setTransparency(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setTransparency, C obj is invalid"); return NULL; }
-	self->objPtr->setTransparency(parseFloat(args));
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::getTransparency(VRPyMaterial* self) {
-    if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::getTransparency, C obj is invalid"); return NULL; }
-    return PyFloat_FromDouble(self->objPtr->getTransparency());
-}
-
-PyObject* VRPyMaterial::setShininess(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setShininess, C obj is invalid"); return NULL; }
-	self->objPtr->setShininess(parseFloat(args));
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::getShininess(VRPyMaterial* self) {
-    if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::getShininess, C obj is invalid"); return NULL; }
-    return PyFloat_FromDouble(self->objPtr->getShininess());
-}
-
-PyObject* VRPyMaterial::setPointSize(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setPointSize, C obj is invalid"); return NULL; }
-	self->objPtr->setPointSize(parseInt(args));
-	Py_RETURN_TRUE;
-}
-
-PyObject* VRPyMaterial::setLineWidth(VRPyMaterial* self, PyObject* args) {
-	if (self->objPtr == 0) { PyErr_SetString(err, "VRPyMaterial::setLineWidth, C obj is invalid"); return NULL; }
-	self->objPtr->setLineWidth(parseInt(args));
-	Py_RETURN_TRUE;
-}
--- src/core/scripting/VRPyMaterial.h
+++ src/core/scripting/VRPyMaterial.h
@@ -9,44 +9,9 @@
 
 struct VRPyMaterial : VRPyBaseT<OSG::VRMaterial> {
     static PyMethodDef methods[];
-
-    static PyObject* addPass(VRPyMaterial* self);
-    static PyObject* remPass(VRPyMaterial* self, PyObject* args);
-    static PyObject* setActivePass(VRPyMaterial* self, PyObject* args);
-
-    static PyObject* setLit(VRPyMaterial* self, PyObject* args);
-    static PyObject* getAmbient(VRPyMaterial* self);
-    static PyObject* setAmbient(VRPyMaterial* self, PyObject* args);
-    static PyObject* getDiffuse(VRPyMaterial* self);
-    static PyObject* setDiffuse(VRPyMaterial* self, PyObject* args);
-    static PyObject* getSpecular(VRPyMaterial* self);
-    static PyObject* setSpecular(VRPyMaterial* self, PyObject* args);
-    static PyObject* getTransparency(VRPyMaterial* self);
-    static PyObject* setTransparency(VRPyMaterial* self, PyObject* args);
-    static PyObject* clearTransparency(VRPyMaterial* self);
-    static PyObject* enableTransparency(VRPyMaterial* self);
-    static PyObject* getShininess(VRPyMaterial* self);
-    static PyObject* setShininess(VRPyMaterial* self, PyObject* args);
+    
     static PyObject* setTexture(VRPyMaterial* self, PyObject* args);
-    static PyObject* setTextureType(VRPyMaterial* self, PyObject* args);
-    static PyObject* getTexture(VRPyMaterial* self, PyObject* args);
-    static PyObject* setDepthTest(VRPyMaterial* self, PyObject* args);
-    static PyObject* setFrontBackModes(VRPyMaterial* self, PyObject* args);
-    static PyObject* setMagMinFilter(VRPyMaterial* self, PyObject* args);
-    static PyObject* setTextureWrapping(VRPyMaterial* self, PyObject* args);
-
-    static PyObject* setPointSize(VRPyMaterial* self, PyObject* args);
-    static PyObject* setLineWidth(VRPyMaterial* self, PyObject* args);
-    static PyObject* setWireFrame(VRPyMaterial* self, PyObject* args);
-    static PyObject* setZOffset(VRPyMaterial* self, PyObject* args);
-    static PyObject* setSortKey(VRPyMaterial* self, PyObject* args);
-    static PyObject* setVertexProgram(VRPyMaterial* self, PyObject* args);
-    static PyObject* setFragmentProgram(VRPyMaterial* self, PyObject* args);
-    static PyObject* setGeometryProgram(VRPyMaterial* self, PyObject* args);
-    static PyObject* setTessControlProgram(VRPyMaterial* self, PyObject* args);
-    static PyObject* setTessEvaluationProgram(VRPyMaterial* self, PyObject* args);
     static PyObject* setShaderParameter(VRPyMaterial* self, PyObject* args);
-    static PyObject* setDefaultVertexShader(VRPyMaterial* self);
 };
 
 #ifndef WITHOUT_AV
