5d28931a8cb3e4361c1e71c934ed40bb8cd0ac22
Victor Haefner <victor.haefner@gmail.com>
2026-05-25 13:49:04 +0200

    refactored some py bindings

--- src/core/scripting/VRPyConstructionKit.cpp
+++ src/core/scripting/VRPyConstructionKit.cpp
@@ -10,73 +10,17 @@ using namespace OSG;
 simpleVRPyType(ConstructionKit, New_ptr);
 
 PyMethodDef VRPyConstructionKit::methods[] = {
-    {"clear", (PyCFunction)VRPyConstructionKit::clear, METH_NOARGS, "Clear everything - clear()" },
-    {"getSnappingEngine", (PyCFunction)VRPyConstructionKit::getSnappingEngine, METH_NOARGS, "Get internal snapping engine - getSnappingEngine()" },
-    {"getSelector", (PyCFunction)VRPyConstructionKit::getSelector, METH_NOARGS, "Get internal selector - getSelector(obj)" },
-    {"getObjects", (PyCFunction)VRPyConstructionKit::getObjects, METH_NOARGS, "Get all objects - [obj] getObjects()" },
-    {"addAnchorType", (PyCFunction)VRPyConstructionKit::addAnchorType, METH_VARARGS, "Add new anchor type - addAnchorType(size, color)" },
-    {"addObjectAnchor", (PyCFunction)VRPyConstructionKit::addObjectAnchor, METH_VARARGS, "Add anchor to object - addObjectAnchor(obj, int anchor, position, flt radius)" },
-    {"addObject", (PyCFunction)VRPyConstructionKit::addObject, METH_VARARGS, "Get internal selector - addObject(obj)" },
-    {"remObject", (PyCFunction)VRPyConstructionKit::remObject, METH_VARARGS, "Get internal selector - remObject(obj)" },
-    {"breakup", (PyCFunction)VRPyConstructionKit::breakup, METH_VARARGS, "Split an object from the system - breakup(obj)" },
+    {"clear", PyWrap( ConstructionKit, clear, "Clear everything - clear()", void ) },
+    {"getSnappingEngine", PyWrap( ConstructionKit, getSnappingEngine, "Get internal snapping engine - getSnappingEngine()", VRSnappingEnginePtr ) },
+    {"getSelector", PyWrap( ConstructionKit, getSelector, "Get internal selector - getSelector(obj)", VRSelectorPtr ) },
+    {"getObjects", PyWrap( ConstructionKit, getObjects, "Get all objects - [obj] getObjects()", vector<VRObjectPtr> ) },
+    {"addAnchorType", PyWrap( ConstructionKit, addAnchorType, "Add new anchor type - addAnchorType(size, color)", int, float, Color3f ) },
+    {"addObjectAnchor", PyWrap( ConstructionKit, addObjectAnchor, "Add anchor to object - addObjectAnchor(obj, int anchor, position, flt radius)", VRGeometryPtr, VRTransformPtr, int, Vec3d, float ) },
+    {"addObject", PyWrap( ConstructionKit, addObject, "Get internal selector - addObject(obj)", void, VRTransformPtr ) },
+    {"remObject", PyWrap( ConstructionKit, remObject, "Get internal selector - remObject(obj)", void, VRTransformPtr ) },
+    {"breakup", PyWrap( ConstructionKit, breakup, "Split an object from the system - breakup(obj)", void, VRTransformPtr ) },
     {"toggleConstruction", PyWrap( ConstructionKit, toggleConstruction, "Toggle construction mode", void, bool ) },
     {NULL}  /* Sentinel */
 };
-
-PyObject* VRPyConstructionKit::getSnappingEngine(VRPyConstructionKit* self) { return VRPySnappingEngine::fromSharedPtr(self->objPtr->getSnappingEngine()); }
-PyObject* VRPyConstructionKit::getSelector(VRPyConstructionKit* self) { return VRPySelector::fromSharedPtr(self->objPtr->getSelector()); }
-PyObject* VRPyConstructionKit::clear(VRPyConstructionKit* self) { self->objPtr->clear(); Py_RETURN_TRUE; }
-
-PyObject* VRPyConstructionKit::getObjects(VRPyConstructionKit* self) {
-    if (!self->valid()) return NULL;
-
-    auto objs = self->objPtr->getObjects();
-    PyObject* res = PyList_New(objs.size());
-    for (unsigned int i=0; i<objs.size(); i++) {
-        PyList_SetItem(res, i, VRPyTypeCaster::cast(objs[i]));
-    }
-
-    return res;
-}
-
-PyObject* VRPyConstructionKit::breakup(VRPyConstructionKit* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-    OSG::VRGeometryPtr geo = 0;
-    if (!VRPyGeometry::parse(args, &geo)) return NULL;
-    self->objPtr->breakup(geo);
-    Py_RETURN_TRUE;
-}
-
-PyObject* VRPyConstructionKit::addAnchorType(VRPyConstructionKit* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-    float f; PyObject* o = 0;
-    if (! PyArg_ParseTuple(args, "fO", &f, &o)) return NULL;
-    return PyLong_FromLong( self->objPtr->addAnchorType(f, Vec3f(parseVec3dList(o))) );
-}
-
-PyObject* VRPyConstructionKit::addObjectAnchor(VRPyConstructionKit* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-    VRPyGeometry* g; PyObject* p;
-    int a; float d;
-    if (! PyArg_ParseTuple(args, "OiOf", &g, &a, &p, &d)) return NULL;
-    auto anc = self->objPtr->addObjectAnchor(g->objPtr, a, parseVec3dList(p), d);
-    return VRPyGeometry::fromSharedPtr(anc);
-}
-
-PyObject* VRPyConstructionKit::addObject(VRPyConstructionKit* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-    OSG::VRGeometryPtr geo = 0;
-    if (!VRPyGeometry::parse(args, &geo)) return NULL;
-    self->objPtr->addObject(geo);
-    Py_RETURN_TRUE;
-}
-
-PyObject* VRPyConstructionKit::remObject(VRPyConstructionKit* self, PyObject* args) {
-    if (!self->valid()) return NULL;
-    OSG::VRGeometryPtr geo = 0;
-    if (!VRPyGeometry::parse(args, &geo)) return NULL;
-    self->objPtr->remObject(geo);
-    Py_RETURN_TRUE;
-}
 
 
--- src/core/scripting/VRPyConstructionKit.h
+++ src/core/scripting/VRPyConstructionKit.h
@@ -6,16 +6,6 @@
 
 struct VRPyConstructionKit : VRPyBaseT<OSG::VRConstructionKit> {
     static PyMethodDef methods[];
-
-    static PyObject* clear(VRPyConstructionKit* self);
-    static PyObject* getSnappingEngine(VRPyConstructionKit* self);
-    static PyObject* getSelector(VRPyConstructionKit* self);
-    static PyObject* getObjects(VRPyConstructionKit* self);
-    static PyObject* addAnchorType(VRPyConstructionKit* self, PyObject* args);
-    static PyObject* addObjectAnchor(VRPyConstructionKit* self, PyObject* args);
-    static PyObject* addObject(VRPyConstructionKit* self, PyObject* args);
-    static PyObject* remObject(VRPyConstructionKit* self, PyObject* args);
-    static PyObject* breakup(VRPyConstructionKit* self, PyObject* args);
 };
 
 #endif // VRPYCONSTRUCTIONKIT_H_INCLUDED
